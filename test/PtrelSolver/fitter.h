32d121f29cbff59f1773b745106ebd587ac559fb
Ping Tan <sha1-21260bfe417a709e6eaff9bef937b0ef7493f858@cern.ch>
2007-12-21 18:01:23 +0000

    *** empty log message ***

---
+++ test/PtrelSolver/fitter.h
@@ -0,0 +1,182 @@
+#ifndef fitter_h
+#define fitter_h
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TCut.h"
+#include "TF1.h"
+#include "TH1F.h"
+#include "TCanvas.h"
+#include "TString.h"
+#include "TMath.h"
+
+#include "math.h"
+#include "fstream.h"
+#include "iomanip.h"
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
+#include "vector.h"
+#include "TObject.h"
+#include "TObjArray.h"
+#include "TGraphErrors.h"
+#include "TLegend.h"
+#include "TLatex.h"
+
+
+Double_t pdf(Double_t *xx, Double_t *par);
+Double_t pdf1(Double_t *xx, Double_t *par);
+Double_t combined_pdf(Double_t *xx, Double_t *par);
+
+static double pthatbining[100];
+static double etabining[100];
+
+class fitter : public TObject {
+
+ protected:
+
+  Int_t pthat_bins;
+  Int_t eta_bins;
+  
+
+  Double_t x_min, x_max;
+  Double_t fit_min, fit_max;
+  Int_t hist_bins;
+
+  Int_t pt_threshold;
+  Int_t pt_sumbins;
+  Int_t eta_threshold;
+  Int_t eta_sumbins;
+
+
+  // define cuts
+  TCut muon, jet;
+
+  TCanvas *c1;
+  std::fstream file;
+  TF1     *func;
+  TH1F    *hist;
+  TFile   *inputfile;
+  TFile   *outfile;
+
+  TLatex  *label;
+
+
+  std::vector<std::vector<double> > *eff_table;
+  std::vector<std::vector<double> > *pdfs_b;
+  std::vector<std::vector<double> > *pdfs_c;
+  TObjArray                          combined_pdfs;
+
+
+  std::vector<std::vector<double> > *pdfs_b_tag;
+  std::vector<std::vector<double> > *pdfs_c_tag;
+  TObjArray                          combined_pdfs_tag;
+
+
+  TObjArray                          combined_pdfs_sys;
+  TObjArray                          combined_pdfs_sys_tag;
+
+
+  TObjArray data, mc;  // store histograms. 
+
+
+  void init();
+
+ public:
+
+  fitter();
+  virtual ~fitter();
+  fitter(int ptbins, int etabins);
+
+
+
+  void   setPtAverage(int threshold, int sum);
+  void   setEtaAverage(int threshold, int sum);
+
+  int    getBin(double xx, double *binning);
+  int    index( double pt, double eta, double others = 0);
+  int    index( int ptnum, int etanum, int others=0);
+  double effErr(double N1, double N1_err, double N2, double N2_err);
+
+
+  TF1   *getAPdf(int ii);
+  TF1   *getAPdf(int pt_bin, int eta_bin);
+  TF1   *getPdfByIndex(int ii);
+  TF1   *getPdfByIndex(int ii, const char *tag);
+  TF1   *getTaggedPdfByIndex(int ii);
+
+
+
+
+  void readTemplates(const char *filename, std::vector<std::vector<double> > *parameters);
+  void buildPdfs(TObjArray *combined, std::vector<std::vector<double> > *b, std::vector<std::vector<double> > *c, const char *tag = 0);
+  TF1 *buildAPdf(int ii, std::vector<std::vector<double> > *b, std::vector<std::vector<double> > *c);
+
+
+  void initPdfs(const char *b_pdf = "b_flavor.data", const char *c_pdf = "c_flavor.data");
+  void initPdfs(const char *b_pdf,  const char *c_pdf, TObjArray *combined, const char *tag);
+
+
+  void makeTemplates(int flavor, const char *inputfilename, const char *pdffilename, const char *rootfilename, const char *tag  ="", const char *pthist = "npT", const char *etahist="nEta", bool latex = false);
+
+  void makeTemplates(int flavor = 5, const char *filename = "b_flavor.dat", const char *rootfilename = "b_pdfs.root" );
+
+  
+
+  TH1F *getData(double pt_min, double pt_max, double eta_min, double eta_max, TCut cut);
+  void getMCEff(double pt_min, double pt_max, double eta_min, double eta_max, TCut tagger, double &eff, double &err);
+
+
+  // build efficiecny basing give data & templates. 
+  void estimate(TCut tagger, 
+		std::vector<std::vector<double> > *eff, 
+		std::vector<std::vector<double> > *b, 
+		std::vector<std::vector<double> > *c,
+		const char *rootfilename = "fit_result.root",
+		const char *filename = "fit_result.dat");
+  void effCal(TH1F *hist,
+	      TH1F *hist_tag, 
+	      TF1  *pdf,
+	      std::vector<double> *eff,
+	      const char *rootfilename);
+  void effCal(TH1F *hist,
+	      TH1F *hist_tag, 
+	      TF1  *pdf,
+	      std::vector<double> *eff);
+  void effCal(TH1F *hist,
+	      TH1F *hist_tag, 
+	      TF1  *pdf,
+	      TF1  *pdf_tag, 
+	      std::vector<double> *eff);
+
+  void makePlot(const char *epsname, TH1 *hist, TF1 *pdf);
+
+  void makeEffTable(std::vector<std::vector<double> > *eff, const char *filename="eff.table");
+  void makeEffHists(std::vector<std::vector<double> > *eff, const char *tag ="run1");
+  void makeHistEPS(TObjArray &data, TObjArray &mc);
+  void test(bool sys= false);
+  void make(bool sys = false);
+  void makettbar();
+
+
+  TH1F *getMCeff(TFile *file, const char *hist); 
+  void measure(const char *inputfilename, const char *outputfilename, const char *tag, const char *pthist, const char *etahist, bool sys);
+
+  void measure(const char *inputfilename, const char *tag, const char *pthist="npT", const char *etahist= "nEta", bool sys= false);
+
+
+
+  void Fit(TH1F *data, TF1 *pdf, std::vector<double> *num, std::vector<double> *num_err);
+  
+
+
+  TGraphErrors *checkLinearity2(TF1 *pdf, int steps, int total_events= 1000, bool verbose = true);
+  TGraphErrors *checkPurity2(TF1 *pdf, int steps, int num_bs, bool verbose);
+
+
+  ClassDef(fitter, 1)
+};
+
+#endif
+
