79012f3ed55fd8eb11a47512388466370f229c17
Gena Kukartsev <sha1-6f9115b624c0369bb0ff1d34cab9b9d238221d16@cern.ch>
2008-02-15 22:27:08 +0000

    added tag consistency method

--- plugins/seal.cc
+++ plugins/seal.cc
@@ -3,9 +3,13 @@
 #include "RecoBTag/PerformanceMeasurements/plugins/TtDilepLRValPlots.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtDilepSolutionFilter.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtBTagAnalysis.h"
+#include "RecoBTag/PerformanceMeasurements/interface/TtObservables.h"
+#include "RecoBTag/PerformanceMeasurements/interface/TtSemiLeptonicTagCounting.h"
 
 DEFINE_SEAL_MODULE();
 DEFINE_ANOTHER_FWK_MODULE(TtDilepLRValPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtDilepLRObsPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtDilepSolutionFilter);
 DEFINE_ANOTHER_FWK_MODULE(TtBTagAnalysis);
+DEFINE_ANOTHER_FWK_MODULE(TtObservables);
+DEFINE_ANOTHER_FWK_MODULE(TtSemiLeptonicTagCounting);
