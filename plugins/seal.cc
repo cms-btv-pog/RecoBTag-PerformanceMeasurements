3451fbd667ca2ffcd7528f7b26c50e59ccb81842
jmmaes <sha1-479d901633b313ce08d9ddc5dc7a37a2d890f4f5@cern.ch>
2008-02-19 10:09:24 +0000

    adapted for LR method, semilep channel

--- plugins/seal.cc
+++ plugins/seal.cc
@@ -3,6 +3,9 @@
 #include "RecoBTag/PerformanceMeasurements/plugins/TtDilepLRValPlots.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtDilepSolutionFilter.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtBTagAnalysis.h"
+#include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepSolutionFilter.h"
+#include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepLRObsPlots.h"
+#include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepLRValPlots.h"
 #include "RecoBTag/PerformanceMeasurements/interface/TtObservables.h"
 #include "RecoBTag/PerformanceMeasurements/interface/TtSemiLeptonicTagCounting.h"
 
@@ -11,5 +14,8 @@ DEFINE_ANOTHER_FWK_MODULE(TtDilepLRValPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtDilepLRObsPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtDilepSolutionFilter);
 DEFINE_ANOTHER_FWK_MODULE(TtBTagAnalysis);
+DEFINE_ANOTHER_FWK_MODULE(TtSemilepSolutionFilter);
+DEFINE_ANOTHER_FWK_MODULE(TtSemilepLRObsPlots);
+DEFINE_ANOTHER_FWK_MODULE(TtSemilepLRValPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtObservables);
 DEFINE_ANOTHER_FWK_MODULE(TtSemiLeptonicTagCounting);
