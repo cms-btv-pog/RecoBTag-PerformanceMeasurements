ae3534689cd5766172dbde73ce4dc7c7400a3575
Gena Kukartsev <sha1-6f9115b624c0369bb0ff1d34cab9b9d238221d16@cern.ch>
2008-03-21 20:53:22 +0000

    *** empty log message ***

--- src/TtSemiLeptonicTagCounting.cc
+++ src/TtSemiLeptonicTagCounting.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Gena Kukartsev, kukarzev@fnal.gov
 //         Created:  Fri Jun 29 14:53:10 CDT 2007
-// $Id: TtSemiLeptonicTagCounting.cc,v 1.1.2.1 2008/02/15 22:27:08 kukartse Exp $
+// $Id: TtSemiLeptonicTagCounting.cc,v 1.1.2.2 2008/03/21 16:59:37 kukartse Exp $
 //
 //
 
@@ -30,6 +30,14 @@ TtSemiLeptonicTagCounting::TtSemiLeptonicTagCounting(const edm::ParameterSet& iC
   dHigh        = iConfig.getParameter<double>("discriminatorHigh");
   dStep        = iConfig.getParameter<double>("discriminatorStep");
 
+  dLow_2         = iConfig.getParameter<double>("discriminatorLow_2");
+  dHigh_2        = iConfig.getParameter<double>("discriminatorHigh_2");
+  dStep_2        = iConfig.getParameter<double>("discriminatorStep_2");
+  
+  dLow_3         = iConfig.getParameter<double>("discriminatorLow_3");
+  dHigh_3        = iConfig.getParameter<double>("discriminatorHigh_3");
+  dStep_3        = iConfig.getParameter<double>("discriminatorStep_3");
+  
   _interFreq   = iConfig.getParameter<int>("intermediateResultFrequency");
   _dataType    = iConfig.getParameter<string>("dataType");
   _lookForGenJetMatch    = iConfig.getParameter<bool>("lookForGenJetMatch");
@@ -39,6 +47,8 @@ TtSemiLeptonicTagCounting::TtSemiLeptonicTagCounting(const edm::ParameterSet& iC
   _muonSource    = iConfig.getParameter<string>("muonSource");
   _METSource    = iConfig.getParameter<string>("METSource");
   _jetTagSource    = iConfig.getParameter<string>("jetTagSource");
+  _jetTagSource_2  = iConfig.getParameter<string>("jetTagSource_2");
+  _jetTagSource_3  = iConfig.getParameter<string>("jetTagSource_3");
 
   //_genJetSource    = iConfig.getParameter<string>("genJetSource");
   _outputFileName    = iConfig.getParameter<string>("outputFileName");
@@ -119,10 +129,20 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
   RooGKCounter nOfGoodUnknownJets;
   RooGKCounter nOfGoodOtherJets; // jet that passes selection and is neither quark nor gluon nor id=0
   map<double,RooGKCounter> nTaggedJets;
+  map<double,RooGKCounter> nTaggedJets_2;
+  map<double,RooGKCounter> nTaggedJets_3;
   map<double,RooGKCounter> nOfTaggedLJets;
   map<double,RooGKCounter> nOfTaggedBJets;
   map<double,RooGKCounter> nOfTaggedCJets;
   map<double,RooGKCounter> nOfTaggedUnknownJets;
+  map<double,RooGKCounter> nOfTaggedLJets_2;
+  map<double,RooGKCounter> nOfTaggedBJets_2;
+  map<double,RooGKCounter> nOfTaggedCJets_2;
+  map<double,RooGKCounter> nOfTaggedUnknownJets_2;
+  map<double,RooGKCounter> nOfTaggedLJets_3;
+  map<double,RooGKCounter> nOfTaggedBJets_3;
+  map<double,RooGKCounter> nOfTaggedCJets_3;
+  map<double,RooGKCounter> nOfTaggedUnknownJets_3;
 
   vector<int> goodCaloJetIndex; // indices of the jets that pass quality cuts
   vector<int> goodCaloJetFlavorID; // flavor of the good jets
@@ -148,10 +168,16 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 	  nOfGoodJets . count();
 
 	  map<double,bool> isTagged;
+	  map<double,bool> isTagged_2;
+	  map<double,bool> isTagged_3;
 	  for(double _d = dLow; _d <= dHigh; _d += 0.5) isTagged[_d] = false;
+	  for(double _d = dLow_2; _d <= dHigh_2; _d += 0.5) isTagged_2[_d] = false;
+	  for(double _d = dLow_3; _d <= dHigh_3; _d += 0.5) isTagged_3[_d] = false;
 
 	  JetTagRef _theJetTagRef = cjet -> getBJetTagRef( _jetTagSource );
-	  
+	  JetTagRef _theJetTagRef_2 = cjet -> getBJetTagRef( _jetTagSource_2 );
+	  JetTagRef _theJetTagRef_3 = cjet -> getBJetTagRef( _jetTagSource_3 );
+
 	  // check if the jet is tagged
 	  if ( _theJetTagRef . isNonnull() )
 	    {
@@ -168,6 +194,47 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 		}
 	    }
 
+	  // check if the jet is tagged (second tagger)
+	  if ( _theJetTagRef_2 . isNonnull() )
+	    {
+	      const JetTag * theJetTag_2 = &*( cjet -> getBJetTagRef( _jetTagSource_2 ) );
+
+	      //DEBUG different taggers' info
+	      //cout << "DEBUG: b-tag discriminator 2 = " << theJetTag_2->discriminator() << endl << endl;
+
+	      //
+	      for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+		{
+		  if ( theJetTag_2 -> discriminator() > _d )
+		    {
+		      nTaggedJets_2[_d] . count(); // check the discriminator
+		      
+		      isTagged_2[_d] = true;
+		    }
+		}
+	    }
+
+	  // check if the jet is tagged (third tagger)
+	  if ( _theJetTagRef_3 . isNonnull() )
+	    {
+	      const JetTag * theJetTag_3 = &*( cjet -> getBJetTagRef( _jetTagSource_3 ) );
+
+	      //DEBUG different taggers' info
+	      //cout << "DEBUG: b-tag discriminator 2 = " << theJetTag_3->discriminator() << endl << endl;
+
+	      //
+	      for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+		{
+		  if ( theJetTag_3 -> discriminator() > _d )
+		    {
+		      nTaggedJets_3[_d] . count(); // check the discriminator
+		      
+		      isTagged_3[_d] = true;
+		    }
+		}
+	    }
+
+
 	  // ---- jet flavor (MC only!!!)
 	  if ( _dataType == "MC" ) 
 	    {
@@ -183,6 +250,14 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 		    {
 		      if ( isTagged[_d] ) nOfTaggedBJets[_d] . count();
 		    }
+		  for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+		    {
+		      if ( isTagged_2[_d] ) nOfTaggedBJets_2[_d] . count();
+		    }
+		  for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+		    {
+		      if ( isTagged_3[_d] ) nOfTaggedBJets_3[_d] . count();
+		    }
 		}
 	      else if ( jetFlavor == 4 )
 		{
@@ -192,6 +267,15 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 		    {
 		      if ( isTagged[_d] ) nOfTaggedCJets[_d] . count();
 		    }
+		  for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+		    {
+		      if ( isTagged_2[_d] ) nOfTaggedCJets_2[_d] . count();
+		    }
+		  for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+		    {
+		      if ( isTagged_3[_d] ) nOfTaggedCJets_3[_d] . count();
+		    }
+		  
 		}
 	      else if ( jetFlavor == 1 || jetFlavor == 2 || jetFlavor == 3 || jetFlavor == 21 )
 		{
@@ -201,6 +285,14 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 		    {
 		      if ( isTagged[_d] ) nOfTaggedLJets[_d] . count();
 		    }
+		  for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+		    {
+		      if ( isTagged_2[_d] ) nOfTaggedLJets_2[_d] . count();
+		    }
+		  for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+		    {
+		      if ( isTagged_3[_d] ) nOfTaggedLJets_3[_d] . count();
+		    }
 		}
 	      else if ( jetFlavor == 0 )
 		{
@@ -209,6 +301,14 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 		  for(double _d = dLow; _d < dHigh+0.0001; _d += dStep) // loop over discriminator values
 		    {
 		      if ( isTagged[_d] ) nOfTaggedUnknownJets[_d] . count();
+		    }
+		  for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+		    {
+		      if ( isTagged_2[_d] ) nOfTaggedUnknownJets_2[_d] . count();
+		    }
+		  for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+		    {
+		      if ( isTagged_3[_d] ) nOfTaggedUnknownJets_3[_d] . count();
 		    }
 		}
 	      else nOfGoodOtherJets . count();
@@ -289,6 +389,24 @@ TtSemiLeptonicTagCounting::analyze(const edm::Event& iEvent, const edm::EventSet
 
 	  Nn[_d][ nTaggedJets[_d] . getCount() ]++;
 	}
+      for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+	{
+	  nOfTaggedPassedBJets_2[_d] . increment( nOfTaggedBJets_2[_d] . getCount() );
+	  nOfTaggedPassedCJets_2[_d] . increment( nOfTaggedCJets_2[_d] . getCount() );
+	  nOfTaggedPassedLJets_2[_d] . increment( nOfTaggedLJets_2[_d] . getCount() );
+	  nOfTaggedPassedUnknownJets_2[_d] . increment( nOfTaggedUnknownJets_2[_d] . getCount() );
+	  
+	  Nn_2[_d][ nTaggedJets_2[_d] . getCount() ]++;
+	}
+      for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+	{
+	  nOfTaggedPassedBJets_3[_d] . increment( nOfTaggedBJets_3[_d] . getCount() );
+	  nOfTaggedPassedCJets_3[_d] . increment( nOfTaggedCJets_3[_d] . getCount() );
+	  nOfTaggedPassedLJets_3[_d] . increment( nOfTaggedLJets_3[_d] . getCount() );
+	  nOfTaggedPassedUnknownJets_3[_d] . increment( nOfTaggedUnknownJets_3[_d] . getCount() );
+	  
+	  Nn_3[_d][ nTaggedJets_3[_d] . getCount() ]++;
+	}
     }  
 
   // This is probably obsolete. It used to be utilized to get some intermediate results
@@ -333,11 +451,19 @@ TtSemiLeptonicTagCounting::beginJob(const edm::EventSetup&)
 
   cout << "Opening output files..." << endl;
   string _logFileName = _outputFileName + ".log";
-  string _tableFileName = _outputFileName + ".tab";
+
+  string _tableFileName = _outputFileName + "_" + _jetTagSource + ".tab";
+  string _tableFileName_2 = _outputFileName + "_" + _jetTagSource_2 + ".tab";
+  string _tableFileName_3 = _outputFileName + "_" + _jetTagSource_3 + ".tab";
+
   string _mcFileName = _outputFileName + ".mc";
   string _mcFileName2 = _outputFileName + ".mc2";
   _outputFile . open( _logFileName . c_str() );
+
   _outputFileTable . open( _tableFileName . c_str() );
+ _outputFileTable_2 . open( _tableFileName_2 . c_str() );
+  _outputFileTable_3 . open( _tableFileName_3 . c_str() );
+
   _outputFileMC . open( _mcFileName . c_str() );
   _outputFileMC2 . open( _mcFileName2 . c_str() );
 
@@ -467,6 +593,104 @@ TtSemiLeptonicTagCounting::endJob() {
 	}
     }
 
+ // table for the second tagger
+  _outputFileTable_2 << "     discr       N_0       N_1       N_2       N_3       N_4       N_b N_btagged       N_c N_ctagged       N_l N_ltagged       N_x N_xtagged" << endl;
+  for(double _d = dLow_2; _d < dHigh_2+0.0001; _d += dStep_2) // loop over discriminator values
+    {
+      sprintf( buf, "%10.2f", _d);
+      _outputFileTable_2 << buf;
+      sprintf( buf, "%10d", Nn_2[_d][0]);
+      _outputFileTable_2 << buf;
+      sprintf( buf, "%10d", Nn_2[_d][1]);
+      _outputFileTable_2 << buf;
+      sprintf( buf, "%10d", Nn_2[_d][2]);
+      _outputFileTable_2 << buf;
+      sprintf( buf, "%10d", Nn_2[_d][3]);
+      _outputFileTable_2 << buf;
+      sprintf( buf, "%10d", Nn_2[_d][4]);
+      _outputFileTable_2 << buf;
+
+      if ( _dataType == "MC" )
+	{
+	  sprintf( buf, "%10d", nOfPassedBJets . getCount() );
+	  _outputFileTable_2 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedBJets_2[_d] . getCount() );
+	  _outputFileTable_2 << buf;
+	  sprintf( buf, "%10d", nOfPassedCJets . getCount() );
+	  _outputFileTable_2 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedCJets_2[_d] . getCount() );
+	  _outputFileTable_2 << buf;
+	  sprintf( buf, "%10d", nOfPassedLJets . getCount() );
+	  _outputFileTable_2 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedLJets_2[_d] . getCount() );
+	  _outputFileTable_2 << buf;
+
+	  sprintf( buf, "%10d", nOfPassedUnknownJets . getCount() );
+	  _outputFileTable_2 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedUnknownJets_2[_d] . getCount() );
+	  _outputFileTable_2 << buf << endl;
+
+	}
+      else
+	{
+	  for ( int i = 0; i < 8; i++ )
+	    {
+	      sprintf( buf, "%10d", 0 );
+	      _outputFileTable_2 << buf;
+	    }
+	  _outputFileTable_2 << endl;
+	}
+    }
+
+  // table for the third tagger
+  _outputFileTable_3 << "     discr       N_0       N_1       N_2       N_3       N_4       N_b N_btagged       N_c N_ctagged       N_l N_ltagged       N_x N_xtagged" << endl;
+  for(double _d = dLow_3; _d < dHigh_3+0.0001; _d += dStep_3) // loop over discriminator values
+    {
+      sprintf( buf, "%10.2f", _d);
+      _outputFileTable_3 << buf;
+      sprintf( buf, "%10d", Nn_3[_d][0]);
+      _outputFileTable_3 << buf;
+      sprintf( buf, "%10d", Nn_3[_d][1]);
+      _outputFileTable_3 << buf;
+      sprintf( buf, "%10d", Nn_3[_d][2]);
+      _outputFileTable_3 << buf;
+      sprintf( buf, "%10d", Nn_3[_d][3]);
+      _outputFileTable_3 << buf;
+      sprintf( buf, "%10d", Nn_3[_d][4]);
+      _outputFileTable_3 << buf;
+
+      if ( _dataType == "MC" )
+	{
+	  sprintf( buf, "%10d", nOfPassedBJets . getCount() );
+	  _outputFileTable_3 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedBJets_3[_d] . getCount() );
+	  _outputFileTable_3 << buf;
+	  sprintf( buf, "%10d", nOfPassedCJets . getCount() );
+	  _outputFileTable_3 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedCJets_3[_d] . getCount() );
+	  _outputFileTable_3 << buf;
+	  sprintf( buf, "%10d", nOfPassedLJets . getCount() );
+	  _outputFileTable_3 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedLJets_3[_d] . getCount() );
+	  _outputFileTable_3 << buf;
+
+	  sprintf( buf, "%10d", nOfPassedUnknownJets . getCount() );
+	  _outputFileTable_3 << buf;
+	  sprintf( buf, "%10d", nOfTaggedPassedUnknownJets_3[_d] . getCount() );
+	  _outputFileTable_3 << buf << endl;
+
+	}
+      else
+	{
+	  for ( int i = 0; i < 8; i++ )
+	    {
+	      sprintf( buf, "%10d", 0 );
+	      _outputFileTable_3 << buf;
+	    }
+	  _outputFileTable_3 << endl;
+	}
+    }
+
   
   if ( _dataType == "MC" )
     {
@@ -512,7 +736,12 @@ TtSemiLeptonicTagCounting::endJob() {
   
 
   _outputFile.close(); 
+
   _outputFileTable.close(); 
+  _outputFileTable_2 . close();
+  _outputFileTable_3 . close();
+
+
   _outputFileMC.close(); 
   _outputFileMC2.close(); 
 
