6c644f9cf2144e792ee9136c02d2587ec0536153
cmsbuild <>
2008-03-21 21:09:18 +0000

    This commit was manufactured by cvs2git to create tag 'RecoBTag- PerformanceMeasurements-gak032108_3'.
    Sprout from RecoBTag-PerformanceMeasurements-ttbar 2008-02-19 15:59:20 UTC jmmaes <sha1-479d901633b313ce08d9ddc5dc7a37a2d890f4f5@cern.ch> 'update of Buidlfile for TtSemilepJetComb_LR.cpp'
    Cherrypick from RecoBTag-PerformanceMeasurements-ttbar_16X 2008-03-21 21:09:17 UTC Gena Kukartsev <sha1-6f9115b624c0369bb0ff1d34cab9b9d238221d16@cern.ch> '*** empty log message ***':
        bin/TtSemilepJetComb_LR.cpp
        bin/TtSemilepJetComb_LRobs.cpp
        interface/RooGKCounter.h
        interface/TtObservables.h
        interface/TtSemiLeptonicTagCounting.h
        plugins/TtSemilepLRObsPlots.cc
        plugins/TtSemilepLRObsPlots.h
        plugins/TtSemilepLRValPlots.cc
        plugins/TtSemilepLRValPlots.h
        plugins/TtSemilepSolutionFilter.cc
        plugins/TtSemilepSolutionFilter.h
        src/RooGKCounter.cc
        src/TtObservables.cc
        src/TtSemiLeptonicTagCounting.cc
        test/tagConsistency/crab_test.cfg
        test/tagConsistency/tagCountingTag.cfg
        test/tagConsistency/tag_consistency.cfg
        test/tagConsistency/test.cfg

---
+++ interface/TtObservables.h
@@ -0,0 +1,145 @@
+// -*- C++ -*-
+//
+// Package:    BTagPerformanceMeasurementFromTop
+// Class:      TtObservables
+// 
+/**\class TtObservables TtObservables.cc TopQuarkAnalysis/BTagPerformanceMeasurementFromTop/src/TtObservables.cc
+
+ Description: Tag counting method for b-,c- and light tagging efficiency measurement with ttbar semileptonic events
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Gena Kukartsev, kukarzev@fnal.gov
+//         Created:  Fri Jun 29 14:53:10 CDT 2007
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoBTag/PerformanceMeasurements/interface/RooGKCounter.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/CaloJet.h"
+//#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
+//#include "DataFormats/MuonReco/interface/Muon.h"
+//#include "DataFormats/METReco/interface/CaloMET.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopJet.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopLepton.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopElectron.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopMuon.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopMET.h"
+#include "DataFormats/BTauReco/interface/JetTag.h"
+#include "DataFormats/BTauReco/interface/JetTagFwd.h"
+
+#include "DataFormats/BTauReco/interface/TrackCountingTagInfo.h"
+#include "DataFormats/BTauReco/interface/TrackCountingTagInfoFwd.h"
+#include "DataFormats/BTauReco/interface/JetTracksAssociation.h"
+
+#include "DataFormats/Common/interface/RefToBase.h"
+
+#include "TH1D.h"
+#include "TH1I.h"
+#include "TFile.h"
+
+#include <fstream>
+
+//
+// class decleration
+//
+
+class TtObservables : public edm::EDAnalyzer
+{
+public:
+  explicit TtObservables(const edm::ParameterSet&);
+  ~TtObservables();
+  
+  
+private:
+  virtual void beginJob(const edm::EventSetup&) ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+  
+  // ----------member data ---------------------------
+  
+  /// histogram list
+  //TList histograms_;
+  /// algorithm 
+  //TopBTagSelectorWorker algo_;
+  
+  int findJetMatch( const reco::Jet & theJet, const std::vector<reco::GenJet> & jets, std::vector<int> & jetIndex, double dRCut );
+  
+  RooGKCounter eventCounter;
+  RooGKCounter selectedEvents;
+
+  map< int, map< int, map< int, int > > > Fijk;
+  map< int, map< int, map< int, map< int, int > > > > Fxijk;
+
+  map< double, map< int, int> > Nn; // number of events with n tagged jets
+
+  double dRCut;
+  int nCaloJetsLow;
+  int nLeptonLow;
+
+  int _interFreq;
+  string _dataType;
+  bool _lookForGenJetMatch;
+  string _jetSource, _electronSource, _muonSource, _METSource, _jetTagSource;
+  string _genJetSource;
+  string _outputFileName;
+
+  double dLow, dHigh, dStep;
+
+  RooGKCounter nOfPassedLJets;
+  RooGKCounter nOfPassedBJets;
+  RooGKCounter nOfPassedCJets;
+  RooGKCounter nOfPassedUnknownJets;
+  map<double,RooGKCounter> nOfTaggedPassedLJets;
+  map<double,RooGKCounter> nOfTaggedPassedBJets;
+  map<double,RooGKCounter> nOfTaggedPassedCJets;
+  map<double,RooGKCounter> nOfTaggedPassedUnknownJets;
+
+  RooGKCounter interCounter; // counts intermediate results
+
+  TFile *file_;
+  TH1D *pt_jet;
+  TH1D *pt_el;
+  TH1D *pt_mu;
+  TH1D *pt_MET;
+  TH1D *et_jet;
+  TH1D *et_el;
+  TH1D *et_mu;
+  TH1D *et_MET;
+  TH1D *eta_jet;
+  TH1D *eta_el;
+  TH1D *eta_mu;
+  TH1D *eta_MET;
+  TH1I *n_jet;
+  TH1I *n_el;
+  TH1I *n_mu;
+  
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
---
+++ interface/TtSemiLeptonicTagCounting.h
@@ -0,0 +1,135 @@
+// -*- C++ -*-
+//
+// Package:    BTagPerformanceMeasurementFromTop
+// Class:      TtSemiLeptonicTagCounting
+// 
+/**\class TtSemiLeptonicTagCounting TtSemiLeptonicTagCounting.cc TopQuarkAnalysis/BTagPerformanceMeasurementFromTop/src/TtSemiLeptonicTagCounting.cc
+
+ Description: Tag counting method for b-,c- and light tagging efficiency measurement with ttbar semileptonic events
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Gena Kukartsev, kukarzev@fnal.gov
+//         Created:  Fri Jun 29 14:53:10 CDT 2007
+// $Id: TtSemiLeptonicTagCounting.h,v 1.1.2.1 2008/02/15 22:27:07 kukartse Exp $
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "RecoBTag/PerformanceMeasurements/interface/RooGKCounter.h"
+#include "DataFormats/JetReco/interface/GenJet.h"
+#include "DataFormats/JetReco/interface/CaloJet.h"
+//#include "DataFormats/EgammaCandidates/interface/PixelMatchGsfElectron.h"
+//#include "DataFormats/MuonReco/interface/Muon.h"
+//#include "DataFormats/METReco/interface/CaloMET.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopJet.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopLepton.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopElectron.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopMuon.h"
+#include "AnalysisDataFormats/TopObjects/interface/TopMET.h"
+#include "DataFormats/BTauReco/interface/JetTag.h"
+#include "DataFormats/BTauReco/interface/JetTagFwd.h"
+
+#include "DataFormats/BTauReco/interface/TrackCountingTagInfo.h"
+#include "DataFormats/BTauReco/interface/TrackCountingTagInfoFwd.h"
+#include "DataFormats/BTauReco/interface/JetTracksAssociation.h"
+
+#include "DataFormats/Common/interface/RefToBase.h"
+
+#include <fstream>
+
+//
+// class decleration
+//
+
+class TtSemiLeptonicTagCounting : public edm::EDAnalyzer
+{
+public:
+  explicit TtSemiLeptonicTagCounting(const edm::ParameterSet&);
+  ~TtSemiLeptonicTagCounting();
+  
+  
+private:
+  virtual void beginJob(const edm::EventSetup&) ;
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+  
+  // ----------member data ---------------------------
+  
+  /// histogram list
+  //TList histograms_;
+  /// algorithm 
+  //TopBTagSelectorWorker algo_;
+  
+  int findJetMatch( const reco::Jet & theJet, const std::vector<reco::GenJet> & jets, std::vector<int> & jetIndex, double dRCut );
+  
+  RooGKCounter eventCounter;
+  RooGKCounter selectedEvents;
+
+  map< int, map< int, map< int, int > > > Fijk;
+  map< int, map< int, map< int, map< int, int > > > > Fxijk;
+
+  map< double, map< int, int> > Nn; // number of events with n tagged jets
+  map< double, map< int, int> > Nn_2; // number of events with n tagged jets, second tagger
+  map< double, map< int, int> > Nn_3; // number of events with n tagged jets, third tagger
+
+  double dRCut;
+  int nCaloJetsLow;
+  int nLeptonLow;
+
+  int _interFreq;
+  string _dataType;
+  bool _lookForGenJetMatch;
+  string _jetSource, _electronSource, _muonSource, _METSource;
+ string _jetTagSource, _jetTagSource_2, _jetTagSource_3;
+  string _genJetSource;
+  string _outputFileName;
+
+  double dLow, dHigh, dStep;
+  double dLow_2, dHigh_2, dStep_2;
+  double dLow_3, dHigh_3, dStep_3;
+
+  RooGKCounter nOfPassedLJets;
+  RooGKCounter nOfPassedBJets;
+  RooGKCounter nOfPassedCJets;
+  RooGKCounter nOfPassedUnknownJets;
+  map<double,RooGKCounter> nOfTaggedPassedLJets;
+  map<double,RooGKCounter> nOfTaggedPassedBJets;
+  map<double,RooGKCounter> nOfTaggedPassedCJets;
+  map<double,RooGKCounter> nOfTaggedPassedUnknownJets;
+  map<double,RooGKCounter> nOfTaggedPassedLJets_2;
+  map<double,RooGKCounter> nOfTaggedPassedBJets_2;
+  map<double,RooGKCounter> nOfTaggedPassedCJets_2;
+  map<double,RooGKCounter> nOfTaggedPassedUnknownJets_2;
+  map<double,RooGKCounter> nOfTaggedPassedLJets_3;
+  map<double,RooGKCounter> nOfTaggedPassedBJets_3;
+  map<double,RooGKCounter> nOfTaggedPassedCJets_3;
+  map<double,RooGKCounter> nOfTaggedPassedUnknownJets_3;
+
+  RooGKCounter interCounter; // counts intermediate results
+
+  ofstream _outputFile; 
+
+  ofstream _outputFileTable; 
+  ofstream _outputFileTable_2;
+  ofstream _outputFileTable_3;
+
+  ofstream _outputFileMC; 
+  ofstream _outputFileMC2; 
+  
+};
+
