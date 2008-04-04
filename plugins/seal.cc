7b8976a516b0713d4f686b2eb4dd10bd3f6499be
Gena Kukartsev <sha1-6f9115b624c0369bb0ff1d34cab9b9d238221d16@cern.ch>
2008-04-04 23:54:48 +0000

    *** empty log message ***

--- plugins/seal.cc
+++ plugins/seal.cc
@@ -4,7 +4,7 @@
 #include "RecoBTag/PerformanceMeasurements/plugins/TtDilepSolutionFilter.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtBTagAnalysis.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepSolutionFilter.h"
-#include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepLRObsPlots.h"
+//#include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepLRObsPlots.h"
 #include "RecoBTag/PerformanceMeasurements/plugins/TtSemilepLRValPlots.h"
 #include "RecoBTag/PerformanceMeasurements/interface/TtObservables.h"
 #include "RecoBTag/PerformanceMeasurements/interface/TtSemiLeptonicTagCounting.h"
@@ -15,7 +15,7 @@ DEFINE_ANOTHER_FWK_MODULE(TtDilepLRObsPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtDilepSolutionFilter);
 DEFINE_ANOTHER_FWK_MODULE(TtBTagAnalysis);
 DEFINE_ANOTHER_FWK_MODULE(TtSemilepSolutionFilter);
-DEFINE_ANOTHER_FWK_MODULE(TtSemilepLRObsPlots);
+//DEFINE_ANOTHER_FWK_MODULE(TtSemilepLRObsPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtSemilepLRValPlots);
 DEFINE_ANOTHER_FWK_MODULE(TtObservables);
 DEFINE_ANOTHER_FWK_MODULE(TtSemiLeptonicTagCounting);
