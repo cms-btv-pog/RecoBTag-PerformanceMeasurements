1a035f80aebbea2c9ad969fa0a2023e76be49ede
Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch>
2008-05-30 21:35:37 +0000

    New Ptrel2d plotter.

--- test/Plotters/PlottersLinkDef.h
+++ test/Plotters/PlottersLinkDef.h
@@ -1,5 +1,6 @@
 #include "RecoBTag/PerformanceMeasurements/test/Plotters/BasePlotter.h"
 #include "RecoBTag/PerformanceMeasurements/test/Plotters/IPSPlotter.h"
+#include "RecoBTag/PerformanceMeasurements/test/Plotters/Ptrel2DPlotter.h"
 
 #ifdef __CINT__
 #pragma link off all globals;
@@ -8,5 +9,6 @@
 
 #pragma link C++ class BasePlotter+;
 #pragma link C++ class IPSPlotter+;
+#pragma link C++ class Ptrel2DPlotter+;
 
 #endif
