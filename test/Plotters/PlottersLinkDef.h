4e13802dfb12c4e566881d5a6d67b163533480d5
cmsbuild <>
2008-06-11 08:47:00 +0000

    This commit was manufactured by cvs2git to create tag 'RecoBTag- PerformanceMeasurements-Tom-210-V00'.
    Sprout from master 2008-06-11 08:46:59 UTC Tommaso Boccali <sha1-ce6f8e33294befdbdd970525cd264b612743e399@cern.ch> 'first commit for 21X version'
    Cherrypick from master 2008-05-17 11:06:20 UTC Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch> 'Adding Plotter helper classes.':
        test/Plotters/PlottersLinkDef.h
    Delete:
        test/Plotters/Ptrel2DPlotter.cc
        test/Plotters/Ptrel2DPlotter.h

--- test/Plotters/PlottersLinkDef.h
+++ test/Plotters/PlottersLinkDef.h
@@ -1,6 +1,5 @@
 #include "RecoBTag/PerformanceMeasurements/test/Plotters/BasePlotter.h"
 #include "RecoBTag/PerformanceMeasurements/test/Plotters/IPSPlotter.h"
-#include "RecoBTag/PerformanceMeasurements/test/Plotters/Ptrel2DPlotter.h"
 
 #ifdef __CINT__
 #pragma link off all globals;
@@ -9,6 +8,5 @@
 
 #pragma link C++ class BasePlotter+;
 #pragma link C++ class IPSPlotter+;
-#pragma link C++ class Ptrel2DPlotter+;
 
 #endif
