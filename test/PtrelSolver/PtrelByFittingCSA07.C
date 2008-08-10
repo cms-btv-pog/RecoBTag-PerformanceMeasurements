84f8599ade0822c4ae21073beff72543ab320e2b
Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch>
2008-08-10 21:54:29 +0000

    Some corrections to the comments.

--- test/PtrelSolver/PtrelByFittingCSA07.C
+++ test/PtrelSolver/PtrelByFittingCSA07.C
@@ -3,10 +3,10 @@ void PtrelByFittingCSA07()
 	// Load the PtrelSolver library
 	gSystem->Load("libPtrelSolver.so");
 	
-	// Create a PtrelTemplateMaker
+	// Create a PtrelSolver by fitting
 	PtrelByFitting solver("templates_qcd.root");
 	
-	// Make templates
+	// Measure the efficiencies
 	solver.solve("/uscmst1b_scratch/lpc1/lpcbtag/pratima/Mar-19-2008/results_QCD.root", "fitting_qcd.root");
 }
 
--- test/PtrelSolver/PtrelByFittingCSA07_10invpb.C
+++ test/PtrelSolver/PtrelByFittingCSA07_10invpb.C
@@ -3,10 +3,10 @@ void PtrelByFittingCSA07_10invpb()
 	// Load the PtrelSolver library
 	gSystem->Load("libPtrelSolver.so");
 	
-	// Create a PtrelTemplateMaker
+	// Create a PtrelSolver by fitting
 	PtrelByFitting solver("templates_qcd_WT_10invpb.root");
 	
-	// Make templates
+	// Measure the efficiencies
 	solver.solve("/uscmst1b_scratch/lpc1/lpcbtag/pratima/Mar-19-2008/results_QCD_WT_10pb.root", "fitting_qcd_WT_10invpb.root");
 }
 
