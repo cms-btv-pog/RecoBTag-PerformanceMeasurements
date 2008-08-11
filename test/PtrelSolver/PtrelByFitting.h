b6cd2d4c42cd12260c2389ea9578e5a65a2923a7
Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch>
2008-08-11 05:11:51 +0000

    Committing new PtrelSolver implementation.

---
+++ test/PtrelSolver/PtrelByFitting.h
@@ -0,0 +1,35 @@
+#ifndef PtrelByFitting_h
+#define PtrelByFitting_h
+
+#include "PtrelSolver.h"
+
+class PtrelByFitting : public PtrelSolver
+{
+
+public:
+
+    //! Default constructor.
+    explicit PtrelByFitting(char const * filename) : PtrelSolver(filename) {}
+
+    //! Measure efficiency
+    virtual void solve(char const *, char const *);
+
+private:
+
+    //! Compute the efficiency
+    bool compute(
+        TH1 *,
+        Flavor::Type dependency,
+        ValueVector const &,
+        CovarianceVector const &,
+        ValueVector const &,
+        CovarianceVector const &
+    );
+
+public:
+
+    ClassDef(PtrelByFitting, 1)
+
+};
+
+#endif
