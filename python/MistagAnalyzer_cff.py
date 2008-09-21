16608a96320821125036be31c7b04fe580559838
cmsbuild <>
2008-09-21 21:12:00 +0000

    This commit was manufactured by cvs2git to create tag 'RecoBTag- PerformanceMeasurements-Sep-21-2008-21X-V2'.
    Sprout from RecoBTag-PerformanceMeasurements-branch_16X 2008-09-21 21:11:59 UTC Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch> 'Adding the TemplatePlots.'
    Cherrypick from master 2008-09-21 11:02:49 UTC Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch> 'Changing to detaR for running in 21X X>6.':
        Makefile
        interface/MistagAnalyzer.h
        plugins/MistagAnalyzer.cc
        python/JetCuts_cfi.py
        python/JetPartonAssoc_cff.py
        python/MistagAnalyzer_cff.py
        python/MuonCuts_cfi.py
        python/PerformanceAnalyzer_cff.py
        python/__init__.py
        src/Makefile
        src/Makefile.macos
        test/Plotters/Makefile
        test/Plotters/Makefile.macos
        test/Plotters/Ptrel2DPlotter.C
        test/S8Solver/Makefile
        test/S8Solver/Makefile.macos
        test/S8Tools/Makefile
        test/S8Tools/Makefile.macos
        test/analysis_cfg.py
        test/testmistagAnamyzer.py

---
+++ python/MistagAnalyzer_cff.py
@@ -0,0 +1,52 @@
+import FWCore.ParameterSet.Config as cms
+from SimTracker.TrackHistory.TrackClassifier_cff import *
+
+
+mistag = cms.EDAnalyzer("MistagAnalyzer",
+    trackClassifier, 
+    useTrackHistory          = cms.bool(True),
+    outputFile               = cms.untracked.string('results_50_80.root'),
+    longLivedDecayLenght     = cms.untracked.double(1e-14),
+    bestMatchByMaxValue      = cms.untracked.bool(True),
+    
+    ntrackMin                = cms.int32(0),
+    selTagger                = cms.int32(2),
+    MaxEta                   = cms.double(2.5),
+    tagCut                   = cms.double(5.3),
+    MinPt                    = cms.double(30.0),
+    vetoPos                  = cms.double(4.0),
+    isdata                   = cms.bool(True),
+    Jets                     = cms.string('iterativeCone5CaloJets'),
+    
+    vertexClusteringDistance = cms.untracked.double(0.0001),
+    trackProducer            = cms.untracked.InputTag("generalTracks"),
+    badD0Pull                = cms.untracked.double(3.0),
+    trackAssociator          = cms.untracked.string('TrackAssociatorByHits'),
+    flavourMatchOption       = cms.string('genParticle'),
+    flavourSource            = cms.InputTag("IC5byValAlgo"),
+    
+    jetIdParameters         = cms.PSet(
+        vetoFlavour         = cms.vstring(),
+        rejectBCSplitting   = cms.bool(False),
+        physicsDefinition   = cms.bool(False),
+        coneSizeToAssociate = cms.double(0.3),
+        fillLeptons         = cms.bool(False),
+        fillHeavyHadrons    = cms.bool(False),
+        fillPartons         = cms.bool(True),
+        mcSource            = cms.string('source')
+    ),
+    
+    trackCNegHPModuleName   = cms.string('negativeTrackCountingHighPur'),
+    trackCNegHEModuleName   = cms.string('negativeTrackCountingHighEffJetTags'),
+    trackCHPModuleName      = cms.string('trackCountingHighPurBJetTags'),
+    
+    jetPModuleName          = cms.string('jetProbabilityBJetTags'),
+    jetPPosModuleName       = cms.string('positiveOnlyJetProbabilityJetTags'),
+    jetPNegModuleName       = cms.string('negativeOnlyJetProbabilityJetTags'),
+    trackCHEModuleName      = cms.string('trackCountingHighEffBJetTags'),
+    
+    combinedSvtxModuleName    = cms.string(''),
+    combinedSvtxNegModuleName = cms.string(''),
+    svtxModuleName            = cms.string('simpleSecondaryVertexBJetTags'),
+    svtxNegModuleName         = cms.string('simpleSecondaryVertexNegativeBJetTags')  
+)
