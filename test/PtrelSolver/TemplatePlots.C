b547e1995bb318f8d161979939261f87890cdb77
Victor Eduardo Bazterra <sha1-54ef19053215ea8475b12680ac86ec0eec09114f@cern.ch>
2008-09-21 21:19:53 +0000

    Adding the TemplatePlots.

---
+++ test/PtrelSolver/TemplatePlots.C
@@ -0,0 +1,128 @@
+void TemplatePlots(const char * filename, Int_t nbins, Int_t columns)
+{
+    gROOT->SetStyle("Plain");
+
+    TFile * file = new TFile(filename);
+
+    MakePlots(file, "n", nbins, columns);
+
+    MakePlots(file, "ntag", nbins, columns, "TCL");
+    MakePlots(file, "ntag", nbins, columns, "TCM");
+    MakePlots(file, "ntag", nbins, columns, "TCT");
+
+    MakePlots(file, "ntag", nbins, columns, "JPL");
+    MakePlots(file, "ntag", nbins, columns, "JPM");
+    MakePlots(file, "ntag", nbins, columns, "JPT");
+
+    MakePlots(file, "p", nbins, columns);
+
+    MakePlots(file, "ptag", nbins, columns, "TCL");
+    MakePlots(file, "ptag", nbins, columns, "TCM");
+    MakePlots(file, "ptag", nbins, columns, "TCT");
+
+    MakePlots(file, "ptag", nbins, columns, "JPL");
+    MakePlots(file, "ptag", nbins, columns, "JPM");
+    MakePlots(file, "ptag", nbins, columns, "JPT");
+
+}
+
+
+void MakePlots(TFile * file, const char * sample, Int_t nbins, Int_t columns = 3, char * tag = 0)
+{
+    char hName[256], clName[256], bName[256];
+
+    if (tag)
+    {
+        sprintf(bName, "%s_%s", sample, tag);
+        sprintf(hName, "TemplatePlots_%s_%s", sample, tag);    
+    }
+    else
+    {
+        sprintf(bName, "%s", sample);
+        sprintf(hName, "TemplatePlots_%s", sample);
+    }
+
+    char pName[256];
+    sprintf(pName, "%s_tt216.png", hName);
+
+    TCanvas * c1 = new TCanvas(bName, hName);
+
+    c1->Divide(columns, ceil((Double_t)(2*nbins)/columns));
+
+    for (Int_t i=0; i<nbins; i++)
+    {
+        c1->cd(i+1);
+        SetName(hName, sample, "pT", tag, i+1);
+        SetName(clName, sample, "pT_cl", tag, i+1);
+        SetName(bName, sample, "pT_b", tag, i+1);
+        MakePlot(file, hName, clName, bName);
+    }
+
+    for (Int_t i=0; i<nbins; i++)
+    {
+        c1->cd(i+nbins+1);
+        SetName(hName, sample, "eta", tag, i+1);
+        SetName(clName, sample, "eta_cl", tag, i+1);
+        SetName(bName, sample, "eta_b", tag, i+1);
+        MakePlot(file, hName, clName, bName);
+    }
+
+    TImage *img = TImage::Create();
+    img->FromPad(c1);
+    img->WriteImage(pName);
+}
+
+
+void SetName(char * name, const char * sample, const char * dependency, const char * tag, Int_t i)
+{
+    if (tag)
+    {
+        sprintf(name, "%s_%s_%s_%d", sample, dependency, tag, i);
+    }
+    else
+    {
+        sprintf(name, "%s_%s_%d", sample, dependency, i);
+    }
+    std::cout << "Processing : " << name << std::endl;
+}
+
+
+void MakePlot(TFile * file, const char * hName, const char * clName, const char * bName)
+{
+    char name[256];
+
+    file->cd("templates");
+
+    sprintf(name, "template_%s", clName);
+    TH1D * clTemplate = (TH1D*) gDirectory->Get(name);
+    sprintf(name, "template_%s", bName);
+    TH1D * bTemplate = (TH1D*) gDirectory->Get(name);
+
+    Double_t clNorm = clTemplate->Integral();
+    Double_t bNorm = bTemplate->Integral();
+
+    clTemplate->GetListOfFunctions()->Delete();
+    clTemplate->SetTitle(hName);
+    clTemplate->SetStats(false);
+    clTemplate->DrawNormalized();
+    bTemplate->GetListOfFunctions()->Delete();
+    bTemplate->DrawNormalized("same");
+
+    file->cd("functions");
+
+    sprintf(name, "function_%s", clName);
+    TF1 * function = (TF1*) gDirectory->Get(name);
+    TH1 * clFunction = function->GetHistogram();
+    clFunction->SetLineColor(kRed);
+    clFunction->Scale(1./clNorm);
+    clFunction->Draw("csame");
+
+    sprintf(name, "function_%s", bName);
+    TF1 * function = (TF1*) gDirectory->Get(name);
+    TH1 * bFunction = function->GetHistogram();
+    bFunction->SetLineColor(kBlue);
+    bFunction->Scale(1./bNorm);
+    bFunction->Draw("csame");
+
+}
+
