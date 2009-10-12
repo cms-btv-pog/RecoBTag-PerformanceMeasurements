faa668caaa4fbddd66758f23ae60b4119bcc97a4
Wolfgang Adam <sha1-9da30f805672329e47aa0c29331b540b8cb182d8@cern.ch>
2009-10-12 09:45:43 +0000

    Moved addJetCollection to the end of the file (automatic copy of default jet settings)

--- python/PM_pat_Layer1_cfg.py
+++ python/PM_pat_Layer1_cfg.py
@@ -17,18 +17,6 @@ process.muonMatch.checkCharge = False
 #-- Extra Jet/MET collections -------------------------------------------------
 from PhysicsTools.PatAlgos.tools.jetTools import *
 
-addJetCollection(process,
-		 cms.InputTag('antikt5CaloJets'),
-                 'AK5',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = ('AK5','Calo'),
-                 doType1MET   = True,
-		 doL1Cleaning = True,
-		 doL1Counters = True, # NOTE, you need to create two paths and treat carefully the counters
-                 genJetCollection=cms.InputTag("antikt5GenJets")
-                 )
-
 #-- Tune contents of jet collections  ------- CHECK Do we need this?
 #for jetName in ( '', 'AK5' ):
 #    module = getattr(process,'allLayer1Jets'+jetName)
@@ -59,7 +47,7 @@ process.allLayer1Jets.embedGenJetMatch = False
 #    cms.InputTag("trackCountingHighPurBJetTags")
 #    )
 #process.allLayer1JetsAK5.addJetID = True
-process.allLayer1JetsAK5.embedGenJetMatch = False
+## process.allLayer1JetsAK5.embedGenJetMatch = False
 #process.allLayer1JetsAK5.tagInfoSources = cms.VInputTag(cms.InputTag("secondaryVertexTagInfos"), cms.InputTag("softMuonTagInfos"), cms.InputTag("impactParameterTagInfos")) # remove softelectronTagInfos
 #process.allLayer1JetsAK5.discriminatorSources = cms.VInputTag(
 #    cms.InputTag("jetProbabilityBJetTags"), 
@@ -78,9 +66,9 @@ process.allLayer1JetsAK5.embedGenJetMatch = False
 
 process.selectedLayer1Muons.cut      = cms.string('pt > 5. & abs(eta) < 2.4 & isGlobalMuon() & innerTrack().numberOfValidHits()> 7')
 process.selectedLayer1Jets.cut = 'pt > 30. & abs(eta) < 2.4'
-process.selectedLayer1JetsAK5.cut = 'pt > 30. & abs(eta) < 2.4'
+## #&#process.selectedLayer1JetsAK5.cut = 'pt > 30. & abs(eta) < 2.4'
 process.countLayer1Jets.minNumber = cms.uint32(2)
-process.countLayer1JetsAK5.minNumber = cms.uint32(2)
+## process.countLayer1JetsAK5.minNumber = cms.uint32(2)
 process.countLayer1Muons.minNumber = cms.uint32(1)
 
 #from BTag algorithm note
@@ -91,10 +79,10 @@ process.jetPartonMatch.maxDeltaR  = cms.double(0.3)
 process.jetPartonMatch.maxDPtRel  = cms.double(99999999999)
 process.jetGenJetMatch.maxDeltaR  = cms.double(0.5)
 process.jetGenJetMatch.maxDPtRel  = cms.double(99999999999)
-process.jetPartonMatchAK5.maxDeltaR  = cms.double(0.3) 
-process.jetPartonMatchAK5.maxDPtRel  = cms.double(99999999999)
-process.jetGenJetMatchAK5.maxDeltaR  = cms.double(0.5)
-process.jetGenJetMatchAK5.maxDPtRel  = cms.double(99999999999)
+## process.jetPartonMatchAK5.maxDeltaR  = cms.double(0.3) 
+## process.jetPartonMatchAK5.maxDPtRel  = cms.double(99999999999)
+## process.jetGenJetMatchAK5.maxDeltaR  = cms.double(0.5)
+## process.jetGenJetMatchAK5.maxDPtRel  = cms.double(99999999999)
 
 
 # deltaR cut filters
@@ -123,8 +111,27 @@ DeltaRCutAK5 = cms.EDFilter("PMDeltaRFilter",
 #    process.selectedLayer1Summary.candidates.append(cms.InputTag('selectedLayer1Jets'+jets))
 #    process.cleanLayer1Summary.candidates.append(cms.InputTag('cleanLayer1Jets'+jets))
 
+#
+# add new jet collections at the end (in order to copy all modifications
+#   to the default jet collection)
+#
+addJetCollection(process,
+		 cms.InputTag('antikt5CaloJets'),
+                 'AK5',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5','Calo'),
+                 doType1MET   = True,
+		 doL1Cleaning = True,
+		 doL1Counters = True, # NOTE, you need to create two paths and treat carefully the counters
+                 genJetCollection=cms.InputTag("antikt5GenJets")
+                 )
+
+
+
 # Sequence
 #process.p = cms.Path( process.patDefaultSequence*process.patTrigger*process.patTriggerEvent )
+
 
 PM_tuple = cms.Sequence( process.patDefaultSequence )
 
