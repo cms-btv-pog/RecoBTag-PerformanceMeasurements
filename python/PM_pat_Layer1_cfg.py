42454694bb6b4478e86c37c6e935a6a20b1bf82d
Mariarosaria D'Alfonso <sha1-99799ac6e1443b3c6b909d88a0ac76a6660aed57@cern.ch>
2010-04-01 15:28:53 +0000

    Configuration for 356

--- python/PM_pat_Layer1_cfg.py
+++ python/PM_pat_Layer1_cfg.py
@@ -8,109 +8,50 @@ from PhysicsTools.PatAlgos.patTemplate_cfg import *
 #-- PAT standard config -------------------------------------------------------
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
-#-- Tuning of Monte Carlo matching --------------------------------------------
-# Also match with leptons of opposite charge
-#process.electronMatch.checkCharge = False
-process.muonMatch.checkCharge = False
-
-
 #-- Extra Jet/MET collections -------------------------------------------------
 from PhysicsTools.PatAlgos.tools.jetTools import *
+from PhysicsTools.PatAlgos.tools.metTools import *
 
-#-- Tune contents of jet collections  ------- CHECK Do we need this?
-#for jetName in ( '', 'AK5' ):
-#    module = getattr(process,'allLayer1Jets'+jetName)
-#    module.addJetID    = True     # Add JetID variables
-#    module.tagInfoSources = cms.VInputTag(cms.InputTag("secondaryVertexTagInfos"), cms.InputTag("softMuonTagInfos"), cms.InputTag("impactParameterTagInfos")) # remove softelectronTagInfos
-#    module.embedGenJetMatch = False # Only keep reference, since we anyway keep the genJet collections
-#    module.discriminatorSources = cms.VInputTag(
-#	cms.InputTag("jetProbabilityBJetTags"), 
-#	cms.InputTag("simpleSecondaryVertexBJetTags"), 
-#        cms.InputTag("softMuonBJetTags"), 
-#	cms.InputTag("softMuonByPtBJetTags"), 
-#	cms.InputTag("softMuonByIP3dBJetTags"), 
-#        cms.InputTag("trackCountingHighEffBJetTags"), 
-#	cms.InputTag("trackCountingHighPurBJetTags")
-#	)
-# something is wrong with the previous loop, that screws up the module name and sequence...
-# let's change them by hand
-#process.allLayer1Jets.addJetID = True
-process.allLayer1Jets.embedGenJetMatch = False
-#process.allLayer1Jets.tagInfoSources = cms.VInputTag(cms.InputTag("secondaryVertexTagInfos"), cms.InputTag("softMuonTagInfos"), cms.InputTag("impactParameterTagInfos")) # remove softelectronTagInfos
-#process.allLayer1Jets.discriminatorSources = cms.VInputTag(
-#    cms.InputTag("jetProbabilityBJetTags"), 
-#    cms.InputTag("simpleSecondaryVertexBJetTags"), 
-#    cms.InputTag("softMuonBJetTags"), 
-#    cms.InputTag("softMuonByPtBJetTags"), 
-#    cms.InputTag("softMuonByIP3dBJetTags"), 
-#    cms.InputTag("trackCountingHighEffBJetTags"), 
-#    cms.InputTag("trackCountingHighPurBJetTags")
-#    )
-#process.allLayer1JetsAK5.addJetID = True
-## process.allLayer1JetsAK5.embedGenJetMatch = False
-#process.allLayer1JetsAK5.tagInfoSources = cms.VInputTag(cms.InputTag("secondaryVertexTagInfos"), cms.InputTag("softMuonTagInfos"), cms.InputTag("impactParameterTagInfos")) # remove softelectronTagInfos
-#process.allLayer1JetsAK5.discriminatorSources = cms.VInputTag(
-#    cms.InputTag("jetProbabilityBJetTags"), 
-#    cms.InputTag("simpleSecondaryVertexBJetTags"), 
-#    cms.InputTag("softMuonBJetTags"), 
-#    cms.InputTag("softMuonByPtBJetTags"), 
-#    cms.InputTag("softMuonByIP3dBJetTags"), 
-#    cms.InputTag("trackCountingHighEffBJetTags"), 
-#    cms.InputTag("trackCountingHighPurBJetTags")
-#    )
+#-----------------------------------------------------  Tune content ----------------------------------------------------------
 
-
-#### PRE-SELECTION
-
-#from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import *
-
-process.selectedLayer1Muons.cut      = cms.string('pt > 5. & abs(eta) < 2.4 & isGlobalMuon() & innerTrack().numberOfValidHits()> 7')
-process.selectedLayer1Jets.cut = 'pt > 30. & abs(eta) < 2.4'
-## #&#process.selectedLayer1JetsAK5.cut = 'pt > 30. & abs(eta) < 2.4'
-process.countLayer1Jets.minNumber = cms.uint32(2)
-## process.countLayer1JetsAK5.minNumber = cms.uint32(2)
-process.countLayer1Muons.minNumber = cms.uint32(1)
+process.patJets.embedGenJetMatch = False # Only keep reference, since we anyway keep the genJet collections
 
 #from BTag algorithm note
 #process.patAODJetTracksAssociator.cut  = "quality('highPurity') && pt > 1 && normalizedChi2 < 5"
 
-#0.3 from BTag note. CHECK DO WE REALLY NEED THIS?
-process.jetPartonMatch.maxDeltaR  = cms.double(0.3) 
-process.jetPartonMatch.maxDPtRel  = cms.double(99999999999)
-process.jetGenJetMatch.maxDeltaR  = cms.double(0.5)
-process.jetGenJetMatch.maxDPtRel  = cms.double(99999999999)
-## process.jetPartonMatchAK5.maxDeltaR  = cms.double(0.3) 
-## process.jetPartonMatchAK5.maxDPtRel  = cms.double(99999999999)
-## process.jetGenJetMatchAK5.maxDeltaR  = cms.double(0.5)
-## process.jetGenJetMatchAK5.maxDPtRel  = cms.double(99999999999)
+#maxDR=0.3 from BTag note. 
+process.patJetPartonMatch.maxDeltaR  = cms.double(0.3) 
+process.patJetPartonMatch.maxDPtRel  = cms.double(99999999999)
+process.patJetGenJetMatch.maxDeltaR  = cms.double(0.5)
+process.patJetGenJetMatch.maxDPtRel  = cms.double(99999999999)
 
+# Also match with leptons of opposite charge
+#process.electronMatch.checkCharge = False
+#process.muonMatch.checkCharge = False
 
-# deltaR cut filters
-DeltaRCut = cms.EDFilter("PMDeltaRFilter",
-     Jets = cms.InputTag("selectedLayer1Jets"),
-     Muons = cms.InputTag("selectedLayer1Muons"),
-     MaxDeltaR = cms.double(0.4)
-)
+# remove the complex tagger at the beginnig to save space: for example remove softelectronTagInfos and combinedSV
 
-#DeltaRCutAK5 = cms.EDFilter("PMDeltaRFilter",
-#     Jets = cms.InputTag("selectedLayer1JetsAK5"),
-#     Muons = cms.InputTag("selectedLayer1Muons"),
-#     MaxDeltaR = cms.double(0.4)
-#)
- 
+process.patJets.tagInfoSources = cms.VInputTag(
+cms.InputTag("secondaryVertexTagInfos"), 
+cms.InputTag("softMuonTagInfos"), 
+cms.InputTag("impactParameterTagInfos")) 
 
-#-- Trigger matching ----------------------------------------------------------
-#from PhysicsTools.PatAlgos.tools.trigTools import *
-#switchOnTrigger( process )
-#process.patTriggerSequence.remove( process.patTriggerMatcher )
-#process.patTriggerEvent.patTriggerMatches  = ()
+process.patJets.discriminatorSources = cms.VInputTag(
+    cms.InputTag("jetProbabilityBJetTags"), 
+    cms.InputTag("simpleSecondaryVertexBJetTags"),
+    cms.InputTag("softMuonBJetTags"), 
+    cms.InputTag("softMuonByPtBJetTags"), 
+    cms.InputTag("softMuonByIP3dBJetTags"), 
+    cms.InputTag("trackCountingHighEffBJetTags"), 
+    cms.InputTag("trackCountingHighPurBJetTags")
+    )
 
+#----------------- Trigger matching ----------------------------------------------------------
+from PhysicsTools.PatAlgos.tools.trigTools import switchOnTrigger
+switchOnTrigger( process )
 
-#for jets in ( 'SC5' ):
-#    process.allLayer1Summary.candidates.append(cms.InputTag('allLayer1Jets'+jets))
-#    process.selectedLayer1Summary.candidates.append(cms.InputTag('selectedLayer1Jets'+jets))
-#    process.cleanLayer1Summary.candidates.append(cms.InputTag('cleanLayer1Jets'+jets))
-
+<<<<<<< PM_pat_Layer1_cfg.py
+=======
 #
 # add new jet collections at the end (in order to copy all modifications
 #   to the default jet collection)
@@ -149,28 +90,86 @@ recoTrackJets   = cms.Sequence(genParticlesForJets * ak5GenJets )
 #                               trackRefsForJets+
 #                               ak5TrackJets #ak7TrackJets
 #                               )
+>>>>>>> 1.9
 
+#----------------- Add other jet collection  ----------------------------------------------------------
 
-from PhysicsTools.PatAlgos.tools.jetTools import *
 addJetCollection(process,
                  cms.InputTag('ak5TrackJets'),
+<<<<<<< PM_pat_Layer1_cfg.py
+                 'AK5', 'Track',
+                 doJTA        = False,
+=======
                  'AKT5',
                  doJTA        = False,
+>>>>>>> 1.9
                  doBTagging   = True,
                  jetCorrLabel = None,
+<<<<<<< PM_pat_Layer1_cfg.py
+                 doType1MET   = False,
+                 doL1Cleaning = False,                 
+=======
                  doType1MET   = True,
                  doL1Cleaning = False,
+>>>>>>> 1.9
                  doL1Counters = False,
                  genJetCollection=cms.InputTag("ak5GenJets"),
-                 doJetID      = False,
-                 jetIdLabel   = "ak5"
+<<<<<<< PM_pat_Layer1_cfg.py
+                 doJetID      = False
                  )
 
+addJetCollection(process,
+                 cms.InputTag('ak5PFJets'),
+                 'AK5', 'PF',
+                 doJTA        = False,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5','PF'),
+                 doType1MET   = False,
+                 doL1Cleaning = False,                 
+                 doL1Counters = False,
+                genJetCollection=cms.InputTag("ak5GenJets"),
+                 doJetID      = False
+=======
+                 doJetID      = False,
+                 jetIdLabel   = "ak5"
+>>>>>>> 1.9
+                 )
 
+#-----------------------------------------------------  Pre-selection ----------------------------------------------------------
+
+# ususally pt(caloJet)> 30 and pt(muon)>5. Now lowered at the beginning
+process.selectedPatMuons.cut      = cms.string('pt > 3. & abs(eta) < 2.4 & isGlobalMuon() & innerTrack().numberOfValidHits()> 7')
+
+process.selectedPatJets.cut = 'pt > 10. & abs(eta) < 2.4'
+process.selectedPatJetsAK5PF.cut = 'pt > 8. & abs(eta) < 2.4'
+process.selectedPatJetsAK5Track.cut = 'pt > 8. & abs(eta) < 2.4'
+
+#process.countPatMuons.minNumber = cms.uint32(1)
+#process.countPatJets.minNumber = cms.uint32(2) # commented to avoid bias against other jet collections
+
+# deltaR cut filters
+#DeltaRCut = cms.EDFilter("PMDeltaRFilter",
+#     Jets = cms.InputTag("selectedPatJets"),
+#     Muons = cms.InputTag("selectedPatMuons"),
+#     MaxDeltaR = cms.double(0.4)
+#)
+
+#DeltaRCutAK5 = cms.EDFilter("PMDeltaRFilter",
+#     Jets = cms.InputTag("selectedPatJetsAK5"),
+#     Muons = cms.InputTag("selectedPatMuons"),
+#     MaxDeltaR = cms.double(0.4)
+#)
+ 
 
 # Sequence
 #process.p = cms.Path( process.patDefaultSequence*process.patTrigger*process.patTriggerEvent )
 
+<<<<<<< PM_pat_Layer1_cfg.py
+
+PM_tuple = cms.Sequence( process.patDefaultSequence )
+
+=======
 PM_tuple = cms.Sequence( process.recoTrackJets *
                          process.patDefaultSequence )
+>>>>>>> 1.9
 
