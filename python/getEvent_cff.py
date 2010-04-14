a3ab18c1242d6d1d2f67d11bae40228f1c0b3090
Mariarosaria D'Alfonso <sha1-99799ac6e1443b3c6b909d88a0ac76a6660aed57@cern.ch>
2010-04-14 09:24:02 +0000

    update bit40 filter

--- python/getEvent_cff.py
+++ python/getEvent_cff.py
@@ -1,7 +1,7 @@
 # Should monitor what is recommended in
 # https://twiki.cern.ch/twiki/bin/view/CMS/Collisions2010Recipes
-# For now used the old
-# https://twiki.cern.ch/twiki/bin/viewauth/CMS/TRKPromptFeedBack#Event_and_track_selection_recipe
+# 
+#http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/DPGAnalysis/Skims/python/MinBiasPDSkim_cfg.py?revision=1.15&view=markup
 #
 
 import FWCore.ParameterSet.Config as cms
@@ -11,9 +11,15 @@ process = cms.Process("SKIM")
 ###### BSC trigger bit
 
 from L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskTechTrigConfig_cff import *
-
 from HLTrigger.HLTfilters.hltLevel1GTSeed_cfi import hltLevel1GTSeed
-bit40 = hltLevel1GTSeed.clone(L1TechTriggerSeeding = cms.bool(True), L1SeedsLogicalExpression = cms.string('40 AND NOT (36 OR 37 OR 38 OR 39)'))
+
+bit40_data = hltLevel1GTSeed.clone(L1TechTriggerSeeding = cms.bool(True),
+                                   L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39) AND NOT ((42 AND NOT 43) OR (43 AND NOT 42))')
+                                   )
+
+bit40_MC = hltLevel1GTSeed.clone(L1TechTriggerSeeding = cms.bool(True),
+                                 L1SeedsLogicalExpression = cms.string('0 AND (40 OR 41) AND NOT (36 OR 37 OR 38 OR 39) AND NOT ((42 AND NOT 43) OR (43 AND NOT 42))')
+                                 )
 
 ###### Physics declared trigger bit
 
@@ -41,6 +47,6 @@ noScraping= cms.EDFilter("FilterOutScraping",
 eventCountProducer = cms.EDProducer("EventCountProducer")
 
 
-getEventDATA = cms.Sequence(bit40*eventCountProducer*physDecl*noScraping*oneGoodVertexFilter)
-getEventMC = cms.Sequence(eventCountProducer*noScraping*oneGoodVertexFilter)
+getEventDATA = cms.Sequence(bit40_data*eventCountProducer*physDecl*noScraping*oneGoodVertexFilter)
+getEventMC = cms.Sequence(bit40_MC*eventCountProducer*noScraping*oneGoodVertexFilter)
 
