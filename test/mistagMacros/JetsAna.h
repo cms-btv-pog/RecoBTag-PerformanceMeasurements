cf7ae880103a1a5feba81f7070c4d926d2e741d4
Jeremy Andrea <sha1-e13c7919dc70be4070a073d895664f51a7d6c0dc@cern.ch>
2010-06-11 14:29:36 +0000

    changes in macros

--- test/mistagMacros/JetsAna.h
+++ test/mistagMacros/JetsAna.h
@@ -1,96 +1,116 @@
 //////////////////////////////////////////////////////////
 // This class has been automatically generated on
-// Mon Apr  6 17:30:18 2009 by ROOT version 5.20/00
-// from TTree Jets/All Jets
-// found on file: results_80_120.root
+// Thu Jun 10 18:18:33 2010 by ROOT version 5.20/00
+// from TTree ttree/ttree
+// found on file: JetTree_All.root
 //////////////////////////////////////////////////////////
 
-#ifndef Jets_h
-#define Jets_h
+#ifndef JetTree_h
+#define JetTree_h
 
 #include <TROOT.h>
 #include <TChain.h>
 #include <TFile.h>
 
-class Jets {
+class JetTree {
 public :
    TTree          *fChain;   //!pointer to the analyzed TTree or TChain
    Int_t           fCurrent; //!current Tree number in a TChain
 
    // Declaration of leaf types
-   Float_t         Event_Njets;
-   Float_t         Event_Trig;
-   Float_t         Jet_Number;
-   Float_t         Jet_Flavour;
-   Float_t         Jet_Ntracks;
-   Float_t         Jet_Pt;
-   Float_t         Jet_Eta;
-   Float_t         Jet_Phi;
-   Float_t         TagN_TC1;
-   Float_t         TagP_TC1;
-   Float_t         TagN_TC2;
-   Float_t         TagP_TC2;
-   Float_t         TagN_TC3;
-   Float_t         TagP_TC3;
-   Float_t         TagN_JP;
-   Float_t         TagP_JP;
-   Float_t         Tag_JP;
-   Float_t         TagN_SSV;
-   Float_t         TagP_SSV;
-   Float_t         TagN_CSV;
-   Float_t         TagP_CSV;
-   Float_t         TagN_MU;
-   Float_t         TagP_MU;
-   Float_t         Cat_TC1;
-   Float_t         Cat_TC2;
-   Float_t         Cat_TC3;
-   Float_t         Cat_JP;
-   Float_t         Cat_SSV;
-   Float_t         Cat_MU;
-   Float_t         Muon_hits;
-   Float_t         Muon_chi2;
-   Float_t         Muon_d0;
-   Float_t         Muon_Pt;
-   Float_t         Muon_PtRel;
+   Int_t           BitTrigger;
+   Int_t           nJet;
+   Int_t           Run;
+   Int_t           Evt;
+   Int_t           LumiBlock;
+   Float_t         Jet_pt[50];   //[nJet]
+   Float_t         Jet_jes[50];   //[nJet]
+   Float_t         Jet_eta[50];   //[nJet]
+   Float_t         Jet_phi[50];   //[nJet]
+   Int_t           Jet_ntracks[50];   //[nJet]
+   Int_t           Jet_flavour[50];   //[nJet]
+   Float_t         Jet_Ip1N[50];   //[nJet]
+   Float_t         Jet_Ip1P[50];   //[nJet]
+   Float_t         Jet_Ip2N[50];   //[nJet]
+   Float_t         Jet_Ip2P[50];   //[nJet]
+   Float_t         Jet_Ip3N[50];   //[nJet]
+   Float_t         Jet_Ip3P[50];   //[nJet]
+   Float_t         Jet_ProbaN[50];   //[nJet]
+   Float_t         Jet_ProbaP[50];   //[nJet]
+   Float_t         Jet_Proba[50];   //[nJet]
+   Float_t         Jet_SvxN[50];   //[nJet]
+   Float_t         Jet_Svx[50];   //[nJet]
+   Int_t           Jet_SvxNTracks[50];   //[nJet]
+   Int_t           Jet_SvxTracks[50];   //[nJet]
+   Float_t         Jet_SvxNHP[50];   //[nJet]
+   Float_t         Jet_SvxHP[50];   //[nJet]
+   Float_t         Jet_CombSvxN[50];   //[nJet]
+   Float_t         Jet_CombSvx[50];   //[nJet]
+   Float_t         Jet_SoftMuN[50];   //[nJet]
+   Float_t         Jet_SoftMu[50];   //[nJet]
+   Int_t           Jet_hist1[50];   //[nJet]
+   Int_t           Jet_hist2[50];   //[nJet]
+   Int_t           Jet_hist3[50];   //[nJet]
+   Int_t           Jet_histJet[50];   //[nJet]
+   Int_t           Jet_histSvx[50];   //[nJet]
+   Int_t           Jet_histMuon[50];   //[nJet]
+   Int_t           Jet_mu_nHit[50];   //[nJet]
+   Float_t         Jet_mu_chi2[50];   //[nJet]
+   Float_t         Jet_mu_pt[50];   //[nJet]
+   Float_t         Jet_mu_ptrel[50];   //[nJet]
+   Int_t           Jet_nFirstTrack[50];   //[nJet]
+   Int_t           Jet_nLastTrack[50];   //[nJet]
+   Int_t           Jet_nFirstSV[50];   //[nJet]
+   Int_t           Jet_nLastSV[50];   //[nJet]
 
    // List of branches
-   TBranch        *b_Event_Njets;   //!
-   TBranch        *b_Event_Trig;   //!
-   TBranch        *b_Jet_Number;   //!
-   TBranch        *b_Jet_Flavour;   //!
-   TBranch        *b_Jet_Ntracks;   //!
-   TBranch        *b_Jet_Pt;   //!
-   TBranch        *b_Jet_Eta;   //!
-   TBranch        *b_Jet_Phi;   //!
-   TBranch        *b_TagN_TC1;   //!
-   TBranch        *b_TagP_TC1;   //!
-   TBranch        *b_TagN_TC2;   //!
-   TBranch        *b_TagP_TC2;   //!
-   TBranch        *b_TagN_TC3;   //!
-   TBranch        *b_TagP_TC3;   //!
-   TBranch        *b_TagN_JP;   //!
-   TBranch        *b_TagP_JP;   //!
-   TBranch        *b_Tag_JP;   //!
-   TBranch        *b_TagN_SSV;   //!
-   TBranch        *b_TagP_SSV;   //!
-   TBranch        *b_TagN_CSV;   //!
-   TBranch        *b_TagP_CSV;   //!
-   TBranch        *b_TagN_MU;   //!
-   TBranch        *b_TagP_MU;   //!
-   TBranch        *b_Cat_TC1;   //!
-   TBranch        *b_Cat_TC2;   //!
-   TBranch        *b_Cat_TC3;   //!
-   TBranch        *b_Cat_JP;   //!
-   TBranch        *b_Cat_SSV;   //!
-   TBranch        *b_Cat_MU;   //!
-   TBranch        *b_Muon_hits;   //!
-   TBranch        *b_Muon_chi2;   //!
-   TBranch        *b_Muon_d0;   //!
-   TBranch        *b_Muon_Pt;   //!
-   TBranch        *b_Muon_PtRel;   //!
+   TBranch        *b_BitTrigger;   //!
+   TBranch        *b_nJet;   //!
+   TBranch        *b_Run;   //!
+   TBranch        *b_Evt;   //!
+   TBranch        *b_LumiBlock;   //!
+   TBranch        *b_Jet_pt;   //!
+   TBranch        *b_Jet_jes;   //!
+   TBranch        *b_Jet_eta;   //!
+   TBranch        *b_Jet_phi;   //!
+   TBranch        *b_Jet_ntracks;   //!
+   TBranch        *b_Jet_flavour;   //!
+   TBranch        *b_Jet_Ip1N;   //!
+   TBranch        *b_Jet_Ip1P;   //!
+   TBranch        *b_Jet_Ip2N;   //!
+   TBranch        *b_Jet_Ip2P;   //!
+   TBranch        *b_Jet_Ip3N;   //!
+   TBranch        *b_Jet_Ip3P;   //!
+   TBranch        *b_Jet_ProbaN;   //!
+   TBranch        *b_Jet_ProbaP;   //!
+   TBranch        *b_Jet_Proba;   //!
+   TBranch        *b_Jet_SvxN;   //!
+   TBranch        *b_Jet_Svx;   //!
+   TBranch        *b_Jet_SvxNTracks;   //!
+   TBranch        *b_Jet_SvxTracks;   //!
+   TBranch        *b_Jet_SvxNHP;   //!
+   TBranch        *b_Jet_SvxHP;   //!
+   TBranch        *b_Jet_CombSvxN;   //!
+   TBranch        *b_Jet_CombSvx;   //!
+   TBranch        *b_Jet_SoftMuN;   //!
+   TBranch        *b_Jet_SoftMu;   //!
+   TBranch        *b_Jet_hist1;   //!
+   TBranch        *b_Jet_hist2;   //!
+   TBranch        *b_Jet_hist3;   //!
+   TBranch        *b_Jet_histJet;   //!
+   TBranch        *b_Jet_histSvx;   //!
+   TBranch        *b_Jet_histMuon;   //!
+   TBranch        *b_Jet_mu_nHit;   //!
+   TBranch        *b_Jet_mu_chi2;   //!
+   TBranch        *b_Jet_mu_pt;   //!
+   TBranch        *b_Jet_mu_ptrel;   //!
+   TBranch        *b_Jet_nFirstTrack;   //!
+   TBranch        *b_Jet_nLastTrack;   //!
+   TBranch        *b_Jet_nFirstSV;   //!
+   TBranch        *b_Jet_nLastSV;   //!
 
-   Jets(TTree *tree=0);
-   virtual ~Jets();
+   JetTree(TTree *tree=0);
+   virtual ~JetTree();
    virtual Int_t    Cut(Long64_t entry);
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
@@ -98,7 +118,7 @@ public :
 //$$
 //$$   void   Loop();
    void   Loop(Int_t    aNN = 0, 
-               Double_t aTagCut = 0.01, 
+               Float_t  aTagCut = 0.01, 
                Float_t  aPtMin = 20., 
                Float_t  aPtMax = 1000., 
                Float_t  aEtaMin = 0., 
@@ -112,35 +132,36 @@ public :
 
 #endif
 
-#ifdef Jets_cxx
-Jets::Jets(TTree *tree)
+#ifdef JetTree_cxx
+JetTree::JetTree(TTree *tree)
 {
 // if parameter tree is not specified (or zero), connect the file
 // used to generate this class and read the Tree.
    if (tree == 0) {
-      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("results_80_120.root");
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("JetTree_All.root");
       if (!f) {
-         f = new TFile("results_80_120.root");
+         f = new TFile("JetTree_All.root");
+         f->cd("JetTree_All.root:/mistag");
       }
-      tree = (TTree*)gDirectory->Get("Jets");
+      tree = (TTree*)gDirectory->Get("ttree");
 
    }
    Init(tree);
 }
 
-Jets::~Jets()
+JetTree::~JetTree()
 {
    if (!fChain) return;
    delete fChain->GetCurrentFile();
 }
 
-Int_t Jets::GetEntry(Long64_t entry)
+Int_t JetTree::GetEntry(Long64_t entry)
 {
 // Read contents of entry.
    if (!fChain) return 0;
    return fChain->GetEntry(entry);
 }
-Long64_t Jets::LoadTree(Long64_t entry)
+Long64_t JetTree::LoadTree(Long64_t entry)
 {
 // Set the environment to read one entry
    if (!fChain) return -5;
@@ -155,7 +176,7 @@ Long64_t Jets::LoadTree(Long64_t entry)
    return centry;
 }
 
-void Jets::Init(TTree *tree)
+void JetTree::Init(TTree *tree)
 {
    // The Init() function is called when the selector needs to initialize
    // a new tree or chain. Typically here the branch addresses and branch
@@ -171,44 +192,54 @@ void Jets::Init(TTree *tree)
    fCurrent = -1;
    fChain->SetMakeClass(1);
 
-   fChain->SetBranchAddress("Event_Njets", &Event_Njets, &b_Event_Njets);
-   fChain->SetBranchAddress("Event_Trig", &Event_Trig, &b_Event_Trig);
-   fChain->SetBranchAddress("Jet_Number", &Jet_Number, &b_Jet_Number);
-   fChain->SetBranchAddress("Jet_Flavour", &Jet_Flavour, &b_Jet_Flavour);
-   fChain->SetBranchAddress("Jet_Ntracks", &Jet_Ntracks, &b_Jet_Ntracks);
-   fChain->SetBranchAddress("Jet_Pt", &Jet_Pt, &b_Jet_Pt);
-   fChain->SetBranchAddress("Jet_Eta", &Jet_Eta, &b_Jet_Eta);
-   fChain->SetBranchAddress("Jet_Phi", &Jet_Phi, &b_Jet_Phi);
-   fChain->SetBranchAddress("TagN_TC1", &TagN_TC1, &b_TagN_TC1);
-   fChain->SetBranchAddress("TagP_TC1", &TagP_TC1, &b_TagP_TC1);
-   fChain->SetBranchAddress("TagN_TC2", &TagN_TC2, &b_TagN_TC2);
-   fChain->SetBranchAddress("TagP_TC2", &TagP_TC2, &b_TagP_TC2);
-   fChain->SetBranchAddress("TagN_TC3", &TagN_TC3, &b_TagN_TC3);
-   fChain->SetBranchAddress("TagP_TC3", &TagP_TC3, &b_TagP_TC3);
-   fChain->SetBranchAddress("TagN_JP", &TagN_JP, &b_TagN_JP);
-   fChain->SetBranchAddress("TagP_JP", &TagP_JP, &b_TagP_JP);
-   fChain->SetBranchAddress("Tag_JP", &Tag_JP, &b_Tag_JP);
-   fChain->SetBranchAddress("TagN_SSV", &TagN_SSV, &b_TagN_SSV);
-   fChain->SetBranchAddress("TagP_SSV", &TagP_SSV, &b_TagP_SSV);
-   fChain->SetBranchAddress("TagN_CSV", &TagN_CSV, &b_TagN_CSV);
-   fChain->SetBranchAddress("TagP_CSV", &TagP_CSV, &b_TagP_CSV);
-   fChain->SetBranchAddress("TagN_MU", &TagN_MU, &b_TagN_MU);
-   fChain->SetBranchAddress("TagP_MU", &TagP_MU, &b_TagP_MU);
-   fChain->SetBranchAddress("Cat_TC1", &Cat_TC1, &b_Cat_TC1);
-   fChain->SetBranchAddress("Cat_TC2", &Cat_TC2, &b_Cat_TC2);
-   fChain->SetBranchAddress("Cat_TC3", &Cat_TC3, &b_Cat_TC3);
-   fChain->SetBranchAddress("Cat_JP", &Cat_JP, &b_Cat_JP);
-   fChain->SetBranchAddress("Cat_SSV", &Cat_SSV, &b_Cat_SSV);
-   fChain->SetBranchAddress("Cat_MU", &Cat_MU, &b_Cat_MU);
-   fChain->SetBranchAddress("Muon_hits", &Muon_hits, &b_Muon_hits);
-   fChain->SetBranchAddress("Muon_chi2", &Muon_chi2, &b_Muon_chi2);
-   fChain->SetBranchAddress("Muon_d0", &Muon_d0, &b_Muon_d0);
-   fChain->SetBranchAddress("Muon_Pt", &Muon_Pt, &b_Muon_Pt);
-   fChain->SetBranchAddress("Muon_PtRel", &Muon_PtRel, &b_Muon_PtRel);
+   fChain->SetBranchAddress("BitTrigger", &BitTrigger, &b_BitTrigger);
+   fChain->SetBranchAddress("nJet", &nJet, &b_nJet);
+   fChain->SetBranchAddress("Run", &Run, &b_Run);
+   fChain->SetBranchAddress("Evt", &Evt, &b_Evt);
+   fChain->SetBranchAddress("LumiBlock", &LumiBlock, &b_LumiBlock);
+   fChain->SetBranchAddress("Jet_pt", Jet_pt, &b_Jet_pt);
+   fChain->SetBranchAddress("Jet_jes", Jet_jes, &b_Jet_jes);
+   fChain->SetBranchAddress("Jet_eta", Jet_eta, &b_Jet_eta);
+   fChain->SetBranchAddress("Jet_phi", Jet_phi, &b_Jet_phi);
+   fChain->SetBranchAddress("Jet_ntracks", Jet_ntracks, &b_Jet_ntracks);
+   fChain->SetBranchAddress("Jet_flavour", Jet_flavour, &b_Jet_flavour);
+   fChain->SetBranchAddress("Jet_Ip1N", Jet_Ip1N, &b_Jet_Ip1N);
+   fChain->SetBranchAddress("Jet_Ip1P", Jet_Ip1P, &b_Jet_Ip1P);
+   fChain->SetBranchAddress("Jet_Ip2N", Jet_Ip2N, &b_Jet_Ip2N);
+   fChain->SetBranchAddress("Jet_Ip2P", Jet_Ip2P, &b_Jet_Ip2P);
+   fChain->SetBranchAddress("Jet_Ip3N", Jet_Ip3N, &b_Jet_Ip3N);
+   fChain->SetBranchAddress("Jet_Ip3P", Jet_Ip3P, &b_Jet_Ip3P);
+   fChain->SetBranchAddress("Jet_ProbaN", Jet_ProbaN, &b_Jet_ProbaN);
+   fChain->SetBranchAddress("Jet_ProbaP", Jet_ProbaP, &b_Jet_ProbaP);
+   fChain->SetBranchAddress("Jet_Proba", Jet_Proba, &b_Jet_Proba);
+   fChain->SetBranchAddress("Jet_SvxN", Jet_SvxN, &b_Jet_SvxN);
+   fChain->SetBranchAddress("Jet_Svx", Jet_Svx, &b_Jet_Svx);
+   fChain->SetBranchAddress("Jet_SvxNTracks", Jet_SvxNTracks, &b_Jet_SvxNTracks);
+   fChain->SetBranchAddress("Jet_SvxTracks", Jet_SvxTracks, &b_Jet_SvxTracks);
+   fChain->SetBranchAddress("Jet_SvxNHP", Jet_SvxNHP, &b_Jet_SvxNHP);
+   fChain->SetBranchAddress("Jet_SvxHP", Jet_SvxHP, &b_Jet_SvxHP);
+   fChain->SetBranchAddress("Jet_CombSvxN", Jet_CombSvxN, &b_Jet_CombSvxN);
+   fChain->SetBranchAddress("Jet_CombSvx", Jet_CombSvx, &b_Jet_CombSvx);
+   fChain->SetBranchAddress("Jet_SoftMuN", Jet_SoftMuN, &b_Jet_SoftMuN);
+   fChain->SetBranchAddress("Jet_SoftMu", Jet_SoftMu, &b_Jet_SoftMu);
+   fChain->SetBranchAddress("Jet_hist1", Jet_hist1, &b_Jet_hist1);
+   fChain->SetBranchAddress("Jet_hist2", Jet_hist2, &b_Jet_hist2);
+   fChain->SetBranchAddress("Jet_hist3", Jet_hist3, &b_Jet_hist3);
+   fChain->SetBranchAddress("Jet_histJet", Jet_histJet, &b_Jet_histJet);
+   fChain->SetBranchAddress("Jet_histSvx", Jet_histSvx, &b_Jet_histSvx);
+   fChain->SetBranchAddress("Jet_histMuon", Jet_histMuon, &b_Jet_histMuon);
+   fChain->SetBranchAddress("Jet_mu_nHit", Jet_mu_nHit, &b_Jet_mu_nHit);
+   fChain->SetBranchAddress("Jet_mu_chi2", Jet_mu_chi2, &b_Jet_mu_chi2);
+   fChain->SetBranchAddress("Jet_mu_pt", Jet_mu_pt, &b_Jet_mu_pt);
+   fChain->SetBranchAddress("Jet_mu_ptrel", Jet_mu_ptrel, &b_Jet_mu_ptrel);
+   fChain->SetBranchAddress("Jet_nFirstTrack", Jet_nFirstTrack, &b_Jet_nFirstTrack);
+   fChain->SetBranchAddress("Jet_nLastTrack", Jet_nLastTrack, &b_Jet_nLastTrack);
+   fChain->SetBranchAddress("Jet_nFirstSV", Jet_nFirstSV, &b_Jet_nFirstSV);
+   fChain->SetBranchAddress("Jet_nLastSV", Jet_nLastSV, &b_Jet_nLastSV);
    Notify();
 }
 
-Bool_t Jets::Notify()
+Bool_t JetTree::Notify()
 {
    // The Notify() function is called when a new file is opened. This
    // can be either for a new TTree in a TChain or when when a new TTree
@@ -219,18 +250,18 @@ Bool_t Jets::Notify()
    return kTRUE;
 }
 
-void Jets::Show(Long64_t entry)
+void JetTree::Show(Long64_t entry)
 {
 // Print contents of entry.
 // If entry is not specified, print current entry
    if (!fChain) return;
    fChain->Show(entry);
 }
-Int_t Jets::Cut(Long64_t entry)
+Int_t JetTree::Cut(Long64_t entry)
 {
 // This function may be called from Loop.
 // returns  1 if entry is accepted.
 // returns -1 otherwise.
    return 1;
 }
-#endif // #ifdef Jets_cxx
+#endif // #ifdef JetTree_cxx
--- test/mistagMacros/JetsAna_Run.C
+++ test/mistagMacros/JetsAna_Run.C
@@ -5,131 +5,46 @@
    gROOT->ProcessLine(".L HistogramManager.C+g") ;
    gROOT->ProcessLine(".L JetsAna.C+g") ;
   
- if (gROOT->GetClass("Jets")==0) return;
+ if (gROOT->GetClass("JetTree")==0) return;
  
- TChain c("Jets");
+ TChain c("mistag/ttree");
 
 ////////////////////////////////////////////////////////////////////////////////
 
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt30_7TeV_SD_Jet50U_1.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_1.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_2.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_3.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_4.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_5.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_6.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_7.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_8.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_9.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_10.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_11.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_12.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_14.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_13.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_14.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_15.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_16.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_17.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_18.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_19.root");
- c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_7TeV_SD_Jet50U_20.root");
-
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt30_SD_Jet50U_1.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_1.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_2.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_3.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_4.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_5.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_6.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_7.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_8.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_9.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_10.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_11.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_12.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_13.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/Ntup_QCD_Pt80_SD_Jet50U_14.root");
-
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_1.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_2.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_3.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_4.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_5.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_6.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_7.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_8.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_9.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_10.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_11.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt80_7TeV_12.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_1.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_2.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_3.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_4.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_5.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_6.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_7.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_8.root");
-//  c.Add("../NTUPLES/CMSSW_3_1_4/NtupTkHist_QCDpt170_7TeV_9.root");
+c.Add("~/NTUPLES/CMSSW_3_6_1_patch3/Data/JetMETTau_Run2010A-PromptReco-v2/JetTree_All.root");
 
 ////////////////////////////////////////////////////////////////////////////////
 
- Jets* t = new Jets(Jets);
+ JetTree* t = new JetTree(&c);
 
-//  t->Loop(0,0.50,30.,999.,0.,2.4,1000.,"output.root");
+ t->Loop(5,2.00,30.,999.,0.,2.4,1000.,"Data-prompt-Jet15U_ptGT30_SSVHPT.root");
+//  t->Loop(2,1.25,30.,999.,0.,2.4,1000.,"output.root");
 
- t->Loop(0,.230,30.,999.,0.0,0.7,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_JPL.root");
- t->Loop(0,.230,30.,999.,0.7,1.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_JPL.root");
- t->Loop(0,.230,30.,999.,1.4,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_JPL.root");
- t->Loop(0,.230,30.,999.,0.0,0.7,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_JPL_veto.root");
- t->Loop(0,.230,30.,999.,0.7,1.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_JPL_veto.root");
- t->Loop(0,.230,30.,999.,1.4,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_JPL_veto.root");
+//  t->Loop(1,3.00,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_IP1.root");
+//  t->Loop(2,1.70,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_TCHEL.root");
+//  t->Loop(2,3.30,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_TCHEM.root");
+//  t->Loop(3,1.93,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_TCHPM.root");
+//  t->Loop(3,3.41,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_TCHPT.root");
+//  t->Loop(4,1.74,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_SSVHEM.root");
+//  t->Loop(4,3.05,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_SSVHET.root");
+//  t->Loop(5,2.00,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_SSVHPT.root");
+//  t->Loop(0,.215,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_JPL.root");
+//  t->Loop(0,.459,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_JPM.root");
+//  t->Loop(0,.669,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_JPT.root");
+//  t->Loop(5,0.38,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_CSVL.root");
+//  t->Loop(5,0.75,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_CSVM.root");
+//  t->Loop(5,.921,0.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_CSVT.root");
 
- t->Loop(0,.495,30.,999.,0.0,0.7,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_JPM.root");
- t->Loop(0,.495,30.,999.,0.7,1.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_JPM.root");
- t->Loop(0,.495,30.,999.,1.4,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_JPM.root");
- t->Loop(0,.495,30.,999.,0.0,0.7,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_JPM_veto.root");
- t->Loop(0,.495,30.,999.,0.7,1.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_JPM_veto.root");
- t->Loop(0,.495,30.,999.,1.4,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_JPM_veto.root");
+//  t->Loop(1,3.00,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_IP1.root");
+//  t->Loop(2,1.70,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_TCHEL.root");
+//  t->Loop(2,3.30,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_TCHEM.root");
+//  t->Loop(3,1.93,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_TCHPM.root");
+//  t->Loop(3,3.41,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_TCHPT.root");
+//  t->Loop(4,1.74,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_SSVHEM.root");
+//  t->Loop(4,3.05,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_SSVHET.root");
+//  t->Loop(5,2.00,30.,999.,0.,2.4,1000.,"output/Data-prompt-Jet15U_ptGT30_SSVHPT.root");
 
- t->Loop(0,.700,30.,999.,0.0,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_JPT.root");
- t->Loop(0,.700,30.,999.,0.0,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_JPT_veto.root");
-
- t->Loop(2,1.90,30.,999.,0.0,0.7,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_TCHEL.root");
- t->Loop(2,1.90,30.,999.,0.7,1.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_TCHEL.root");
- t->Loop(2,1.90,30.,999.,1.4,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_TCHEL.root");
- t->Loop(2,1.90,30.,999.,0.0,0.7,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_TCHEL_veto.root");
- t->Loop(2,1.90,30.,999.,0.7,1.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_TCHEL_veto.root");
- t->Loop(2,1.90,30.,999.,1.4,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_TCHEL_veto.root");
-
- t->Loop(2,3.99,30.,999.,0.0,0.7,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_TCHEM.root");
- t->Loop(2,3.99,30.,999.,0.7,1.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_TCHEM.root");
- t->Loop(2,3.99,30.,999.,1.4,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_TCHEM.root");
- t->Loop(2,3.99,30.,999.,0.0,0.7,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_TCHEM_veto.root");
- t->Loop(2,3.99,30.,999.,0.7,1.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_TCHEM_veto.root");
- t->Loop(2,3.99,30.,999.,1.4,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_TCHEM_veto.root");
-
- t->Loop(3,2.17,30.,999.,0.0,0.7,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_TCHPM.root");
- t->Loop(3,2.17,30.,999.,0.7,1.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_TCHPM.root");
- t->Loop(3,2.17,30.,999.,1.4,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_TCHPM.root");
- t->Loop(3,2.17,30.,999.,0.0,0.7,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_TCHPM_veto.root");
- t->Loop(3,2.17,30.,999.,0.7,1.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_TCHPM_veto.root");
- t->Loop(3,2.17,30.,999.,1.4,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_TCHPM_veto.root");
-
- t->Loop(3,4.31,30.,999.,0.0,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_TCHPT.root");
- t->Loop(3,4.31,30.,999.,0.0,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_TCHPT_veto.root");
-
- t->Loop(4,2.02,30.,999.,0.0,0.7,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_SSVM.root");
- t->Loop(4,2.02,30.,999.,0.7,1.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_SSVM.root");
- t->Loop(4,2.02,30.,999.,1.4,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_SSVM.root");
- t->Loop(4,2.02,30.,999.,0.0,0.7,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_etaLT07_SSVM_veto.root");
- t->Loop(4,2.02,30.,999.,0.7,1.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_07eta14_SSVM_veto.root");
- t->Loop(4,2.02,30.,999.,1.4,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_14eta24_SSVM_veto.root");
-
- t->Loop(4,3.40,30.,999.,0.0,2.4,1000.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_SSVT.root");
- t->Loop(4,3.40,30.,999.,0.0,2.4,20.,"output/QCD_Pt30-80_7TeV_SD_Jet50U_SSVT_veto.root");
+////////////////////////////////////////////////////////////////////////////////
 
  gSystem->Exec("kill -9 "+TString(Form("%d",gSystem->GetPid())));
-
-////////////////////////////////////////////////////////////////////////////////
 }
