c05c8bd3204d9ce4181109f9bde22f2846fcc698
Samvel Khalatyan <sha1-ae95162fd70683bc31ca18885c4c50db0feed2df@cern.ch>
2010-10-26 19:47:47 +0000

    Code cleanup

--- python/TreeMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-TreeMaker = cms.EDAnalyzer(
-    'TreeMaker',
-
-    primaryVertices = cms.string("offlinePrimaryVertices"),
-    muons = cms.string("selectedPatMuonsForPtRel"),
-    jets = cms.string("selectedPatJetsAK5PF")
-)
