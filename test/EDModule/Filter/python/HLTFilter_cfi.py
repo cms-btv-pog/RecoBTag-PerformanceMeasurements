48ab66a892eb8f95817362c152befb1935d417ef
Samvel Khalatyan <sha1-ae95162fd70683bc31ca18885c4c50db0feed2df@cern.ch>
2011-05-23 20:33:12 +0000

    Remove EDModule

--- test/EDModule/Filter/python/HLTFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-HLTFilter = cms.EDFilter(
-    'HLTFilter',
-
-    tag = cms.string("TriggerResults::REDIGI"),
-    hlt = cms.string("HLT_Mu9")
-)
--- test/EDModule/Filter/python/PVFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-PVFilter = cms.EDFilter(
-    'PVFilter',
-
-    pvTag = cms.string("offlinePrimaryVertices"),
-    isDataInput = cms.bool(False)
-)
