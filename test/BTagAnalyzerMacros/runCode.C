403bf582473ae6273bc6903b05ed8cc8f09c28f0
Caroline <caroline.collard@cern.ch>
2015-03-24 08:52:07 +0100

    update files for 7_4_X BTagAnalyzer structure

--- test/BTagAnalyzerMacros/runCode.C
+++ test/BTagAnalyzerMacros/runCode.C
@@ -5,74 +5,29 @@
     TChain *superTree = new TChain("btagana/ttree");
 
     int config_ch = 2;
-    cout << " which config? Phys14=1, CSA14=2, 8TeV=3 " << endl;
+    cout << " which config? from 1 to 6 for QCD and from 7 to 9 for TTbar " << endl;
     cin >> config_ch ;
     cout << " config choisie " << config_ch << endl;
     // MC
-    if (config_ch==1) {
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/Phys14_wtrk/QCD_Pt-30to50_Tune4C_13TeV_pythia8/0000/JetTree_phys14_*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/Phys14_wtrk/QCD_Pt-50to80_Tune4C_13TeV_pythia8/0000/JetTree_phys14_*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/Phys14_wtrk/QCD_Pt-80to120_Tune4C_13TeV_pythia8/0000/JetTree_phys14_*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/Phys14_wtrk/QCD_Pt-120to170_Tune4C_13TeV_pythia8/0000/JetTree_phys14_*.root");
-
-    }
-    else if (config_ch==2) {
-      // CSA14
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-30to50_Tune4C_13TeV_pythia8/0000/JetTree_csa14*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-50to80_Tune4C_13TeV_pythia8/0000/JetTree_csa14*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-80to120_Tune4C_13TeV_pythia8/0000/JetTree_csa14*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-120to170_Tune4C_13TeV_pythia8/0000/JetTree_csa14*.root");
-    }
-    else if (config_ch==3) {
-      // 8 TeV
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/QCD_Pt-30to50_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/QCD_Pt-30to50_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/JetTree_mc_*root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/QCD_Pt-50to80_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/JetTree_mc_*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/QCD_Pt-80to120_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/JetTree_mc_*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/QCD_Pt-120to170_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/JetTree_mc_*.root");
-
-    }
-    else if (config_ch==4) {
-      // CSA14 Pythia6
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-30to50_TuneZ2star_13TeV_pythia6/0000/JetTree_csa14*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-50to80_TuneZ2star_13TeV_pythia6/0000/JetTree_csa14*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-80to120_TuneZ2star_13TeV_pythia6/0000/JetTree_csa14*.root");
-      superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/CSA14/newProd/QCD_Pt-120to170_TuneZ2star_13TeV_pythia6/0000/JetTree_csa14*.root");
-
-    }
-    else if (config_ch==5) {
-      // 8 Tev Pythia6
-      superTree->Add("/opt/sbg/cms/ui8_data1/ccollard/BTAG/MC_Dan_2013April/PythiaQCD/QCD_Pt-30to50_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/TrackTree_*.root");
-      superTree->Add("/opt/sbg/cms/ui8_data1/ccollard/BTAG/MC_Dan_2013April/PythiaQCD/QCD_Pt-50to80_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/TrackTree_*.root");
-      superTree->Add("/opt/sbg/cms/ui8_data1/ccollard/BTAG/MC_Dan_2013April/PythiaQCD/QCD_Pt-80to120_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/TrackTree_*.root");
-      superTree->Add("/opt/sbg/cms/ui8_data2/ccollard/BTAG/MC_Dan_2013April/PythiaQCD/QCD_Pt-120to170_Inclusive_8TeV_pythia6_Summer12_DR53X-PU_S10_AODSIM/TrackTree_*.root");
-
-    }
-    else if (config_ch==6) {
-      // 8 Tev Pythia8
-      superTree->Add("/opt/sbg/data/data2/cms/ccollard/BTAG/prod_pythia8/pythia8_hadd/JetTree_mc_qcd30.root");
-      superTree->Add("/opt/sbg/data/data2/cms/ccollard/BTAG/prod_pythia8/pythia8_hadd/JetTree_mc_qcd50.root");
-      superTree->Add("/opt/sbg/data/data2/cms/ccollard/BTAG/prod_pythia8/pythia8_hadd/JetTree_mc_qcd80.root");
-      superTree->Add("/opt/sbg/data/data2/cms/ccollard/BTAG/prod_pythia8/pythia8_hadd/JetTree_mc_qcd120.root");
-    }
+    if (config_ch==1) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/QCD/QCD50to80_8TeVPythia6_NoPU.root");
+    else if (config_ch==2) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/QCD/QCD50to80_8TeVPythia6_PU.root");
+    else if (config_ch==3) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/QCD/QCD50to80_8TeVPythia8_NoPU.root");
+    else if (config_ch==4) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/QCD/QCD50to80_8TeVPythia8_PU.root");
+    else if (config_ch==5) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/QCD/QCD50to80_13TeVPythia8_NoPU.root");
+    else if (config_ch==6) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/QCD/QCD50to80_13TeVPythia8_PU.root");
+    else if (config_ch==7) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/TTbar/TTbar_8TeVPythia6_PU.root");
+    else if (config_ch==8) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/TTbar/TTbar_8TeVPythia8_NoPU.root");
+    else if (config_ch==9) superTree->Add("/opt/sbg/data/data5/cms/ccollard/BTag/BTagAnalyzerNtuples/RunIWinter15DR/TTbar/TTbar_13TeVPythia8_NoPU.root");
 
 
-    // JetProbaTree : Oui
-    // NewAlgoTree : Non
-    if (config_ch!=3 && config_ch<5) {
-      CommPlotProducer m(superTree,1,1);
-    }
-    else {
-      CommPlotProducer m(superTree,1,1,8);
-    }
+    CommPlotProducer m(superTree);
 
-    // Provide MC information. use SetXS(TString generator, bool qcdtype, int TeV) ;
+
+    // Setting up of the information
     // with generator = pythia or herwig, qcdtype =0 for inclusive or 1 for MuEnriched, and TeV = 8 or 13.
-    if (config_ch!=3 && config_ch<5) {
-        m.SetInfo("pythia",0,13);
-    }
-    else {
-        m.SetInfo("pythia",0,8);
-    }
+    if (config_ch<5) m.SetInfo("pythia",0,8);
+    else if (config_ch==5 || config_ch==6)  m.SetInfo("pythia",0,13);
+    else m.SetInfo("pythia",0,-1);
  
     // Here put the number of events for each samples you are running on.
     // BE CAREFULL !!!!!
@@ -98,85 +53,31 @@
     // m.Counter(); --> TAKES 4 HOURS ON DATA+MC :(
 
    
-    double   n20    = 0.;
-    double   n30    = 0.; 
-    double   n50    = 0.;
-    double   n80    = 0.;
-    double   n120   = 0.; 
-    double   n170   = 0.;
-    double   n300   = 0.;
-    double   n470   = 0.; 
-    double   n600   = 0.;  
-    double   n800   = 0.;  
-    double   n1000  = 0.;
+    if (config_ch==1)      m.Fill_nevent(0.,0.,0., 99360.,0.,0.,0.,0.,0.,0.,0.,0.);
+    else if (config_ch==2) m.Fill_nevent(0.,0.,0., 99360.,0.,0.,0.,0.,0.,0.,0.,0.);
+    else if (config_ch==3) m.Fill_nevent(0.,0.,0.,100000.,0.,0.,0.,0.,0.,0.,0.,0.);
+    else if (config_ch==4) m.Fill_nevent(0.,0.,0.,100000.,0.,0.,0.,0.,0.,0.,0.,0.);
+    else if (config_ch==5) m.Fill_nevent(0.,0.,0.,100000.,0.,0.,0.,0.,0.,0.,0.,0.);
+    else if (config_ch==6) m.Fill_nevent(0.,0.,0.,100000.,0.,0.,0.,0.,0.,0.,0.,0.);
 
-    if (config_ch==1) { 
-        n30   = 498559; 
-        n50   = 517391; 
-        n80   = 924531; 
-        n120   = 1015701; 
+    if (config_ch<7)  {
+      m.SetXS();     // Assign the correct x-sections to QCD pthat bins, depending on SetInfo(), default = use inclusive pythia x-sections for 8 TeV.
+      m.SetSumXS();  // Compute the Total x-section of all the samples
     }
-    else if (config_ch==2) { 
-        n30   = 861636; 
-        n50   = 831797; 
-        n80   = 866767; 
-        n120   = 746130; 
-    }
-    else if (config_ch==3) { 
-        n30   = 5998027; 
-        n50   = 5994786; 
-        n80   = 5986846; 
-        n120   = 5970933; 
-    }
-    else if (config_ch==4) {
-        n30   = 547116; 
-        n50   = 588659;
-        n80   = 600000;
-        n120   = 596768;
-    }
-    else if (config_ch==5) {
-        n30    = 5.96571e+06;
-        n50    = 5.98172e+06;
-        n80   = 5.89208e+06;
-        n120  = 5.91732e+06;
-    }   
-    else if (config_ch==6) {
-        n30   = 1000080;
-        n50    = 1000026;
-        n80   =  1000054;
-        n120  =   800064;
-    }
-
-
-    m.Fill_nevent(0.,n20,n30,n50,n80,n120,n170,n300,n470,n600,n800,n1000);
    
-
-    // Set.XS() will automatically put the cross sections depending on the choice make above with SetInfo().
-    // if nothing was defined, the default is to use inclusive pythia x-sections for 8 TeV.
-    m.SetXS();
- 
-    // Compute the Total x-section of all the samples
-    m.SetSumXS();  
-   
-    // Here put the name of the PU data root file for the PU reweighting
-    //TString PUdataFile="PileUp_jet_abcd.root";
-
-    // For the PU reweighting, you can use the 2012 MC distribution with PU sceanrio S7 (SetPU2012_S7(PUdataFile)),
-    // with PU scenario S10 (SetPU2012_S10(PUdataFile)), or with your own PU vector. Be carefull this vector has to be
-    // of size 60.
-   
-    //m.SetPU2012_S10(PUdataFile);
-   
-    //The loop on tthe events takes 4 inputs: the trigger, the ptmin of the jets, the ptmax of the jets, and the output
-    //file name 
+    // no PU info
    
     TString name_root;
-    if (config_ch==1) {  name_root = "phys14"; }
-    else if (config_ch==2) {  name_root = "csa14"; }
-    else if (config_ch==3) {  name_root = "8tev_new"; }
-    else if (config_ch==4) {  name_root = "csa14_pythia6"; }
-    else if (config_ch==5) {  name_root = "8tev_pythia6"; }
-    else if (config_ch==6) {  name_root = "8tev_pythia8"; }
-    m.Loop("jet", 20, 30, 200,   name_root);
+    if (config_ch==1)      {  name_root = "QCD_8P6NoPU"; }
+    else if (config_ch==2) {  name_root = "QCD_8P6wPU"; }
+    else if (config_ch==3) {  name_root = "QCD_8P8NoPU"; }
+    else if (config_ch==4) {  name_root = "QCD_8P8wPU"; }
+    else if (config_ch==5) {  name_root = "QCD_13P8NoPU"; }
+    else if (config_ch==6) {  name_root = "QCD_13P8wPU"; }
+    else if (config_ch==7) {  name_root = "TT_8P6wPU"; }
+    else if (config_ch==8) {  name_root = "TT_8P8NoPU"; }
+    else if (config_ch==9) {  name_root = "TT_13P8NoPU"; }
+
+    m.Loop("jet", 20, 30, 80,   name_root);
    
 }
--- test/BTagAnalyzerMacros/runPlot.C
+++ test/BTagAnalyzerMacros/runPlot.C
@@ -2,65 +2,24 @@
 {
 gROOT->ProcessLine(".L DrawCommPlot.C++");
 
-DrawMCCgen("pt_hat","pt hat",1)
-DrawMCgen("pt_hat","pt hat",1)
-DrawMCgen("pt_hat_fin1","pt hat",1)
-DrawMCgen("pt_hat_fin2","pt hat",1)
-DrawMCCompare("jet1_ptgen","gen pT",1)
-DrawMCCompare("jet2_ptgen","gen pT",1)
-DrawMCgen("nPU_mc","nPU",1)
-DrawMCgen("nPV_mc","nPV",1)
+DrawMCgen("pt_hat","pt hat",1);
+DrawMCgen("pt_hat_fin","pt hat",1);
+DrawMCCompare("jet_ptgen","gen pT",1);
+DrawMCgen("nPU_mc","nPU",1);
+DrawMCgen("nPV_mc","nPV",1);
 
-DrawMCCompare("jet2_pt_all","reco pT",1)
-DrawMCCompare("jet1_pt_all","reco pT",1)
-DrawMCCompare("jet2_eta","eta",1)
-DrawMCCompare("jet2_eta","eta",0)
-DrawMCCompare("jet1_eta","eta",0)
-DrawMCCompare("jet1_diffpt","diff pt",0)
-DrawMCCompare("jet1_diffrel","rel diff pt",0)
-DrawMCCompare("jet1_diffrel","rel diff pt",1)
-DrawMCCompare("jet2_diffrel","rel diff pt",1)
-DrawMCCompare("jet2_diffpt","diff pt",1)
-DrawMCCompare("CSV_1","CSV",1)
-DrawMCCompare("TCHE_1","TCHE",1)
-DrawMCCompare("JP_1","JP",1)
-DrawMCCompare("CSVIVF_1","CSVv2 w/ IVF",1)
+DrawMCCompare("jet_pt_all","reco pT",1);
+DrawMCCompare("jet_eta","eta",0);
+
+DrawMCCompare("CSV","CSV",1);
+DrawMCCompare("TCHE","TCHE",1);
+DrawMCCompare("JP","JP",1);
+DrawMCCompare("CSVIVF","CSVv2 w/ IVF",1);
+
+DrawMCCompare("track_multi"  ,      "number of tracks in the jets",0);
+DrawMCCompare("trk_multi_sel"  ,    "number of selected tracks in the jets",0);
 
 
-/*
-gROOT->ProcessLine(".L DrawCommPlot.C++");
 DrawMCgen("jet_pt_all_bfromg"   ,"pT of b jets from GSPL",1);
-DrawMCgen("jet_pt_all_c"   ,"pT of c jets",1);
-DrawMCgen("jet_pt_all_cfromg"   ,"pT of c jets from GSPL",1);
-DrawMCgen("jet_pt_all_l"   ,"pT of l jets",1);
-DrawMCgen("jet_pt_all_b"   ,"pT of b jets",1);
-DrawMCgen("jet_pt_all_bfromg"   ,"pT of b jets from GSPL",1);
-DrawMCgen("jet_pt_all_l"   ,"pT of l jets",1);
-DrawMCgen("jet_pt_all_c"   ,"pT of c jets",1);
-DrawMCgen("jet_pt_all_cfromg"   ,"pT of c jets from GSPL",1);
-DrawMCCompare("jet_pt_all"   ,"pT of all jets",1);
-DrawMCCompare("jet_pt_all"   ,"pT of all jets",0);
-DrawMCgen("jet_pt_all_cfromg"   ,"pT of c jets from GSPL",0);
-DrawMCgen("jet_pt_all_c"   ,"pT of c jets",0);
-DrawMCgen("jet_pt_all_l"   ,"pT of l jets",0);
-DrawMCgen("jet_pt_all_bfromg"   ,"pT of b jets from GSPL",0);
-DrawMCgen("jet_pt_all_b"   ,"pT of b jets",0);
-        DrawMCCompare("track_multi"  ,      "number of tracks in the jets",0);
-        DrawMCCompare("trk_multi_sel"  ,    "number of selected tracks in the jets",0);
-        DrawMCCompare("muon_multi"   ,      "number of muons", 1);
-        DrawMCCompare("muon_multi_sel"   ,  "number of selected muons",1);
-        DrawMCCompare("mu_ptrel"     ,      "p_{T} rel. of the muon",0);
-        DrawMCCompare("mu_chi2"      ,      "norm. #chi^{2} of the muon", 1);
-        DrawMCCompare("muon_Pt",             "Muon p_{T}",1);
-        DrawMCCompare("muon_eta",            "Muon #eta",0);
-        DrawMCCompare("muon_phi",            "Muon #phi",0);
-        DrawMCCompare("muon_Ip3d",           "Muon 3D IP",1);
-        DrawMCCompare("muon_Ip2d",           "Muon 2D IP",1);
-        DrawMCCompare("muon_Sip3d",          "Muon 3D IP significance",1);
-        DrawMCCompare("muon_Sip2d",          "Muon 2D IP significance",1);
-        DrawMCCompare("muon_DeltaR",         "Muon #Delta R",0);
-*/
-
-
 
 }
