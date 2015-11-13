2177af00db1efe13dce09b9fa80217cea114dac6
Ivan Marchesini <ivan.marchesini@desy.de>
2015-11-13 14:40:37 +0100

    Not printing dump python config by default

--- test/runBTagAnalyzer_cfg.py
+++ test/runBTagAnalyzer_cfg.py
@@ -1087,4 +1087,4 @@ process.p = cms.Path(
 # Delete predefined output module (needed for running with CRAB)
 del process.out
 
-open('pydump.py','w').write(process.dumpPython())
+#open('pydump.py','w').write(process.dumpPython())
