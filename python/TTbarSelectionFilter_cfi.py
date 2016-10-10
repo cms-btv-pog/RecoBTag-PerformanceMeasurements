461715daaa638fec522613bbd021d94f7ed2ee3c
Kittikul Kovitanggoon <kovitang.cern@gmail.com>
2016-10-10 17:12:05 +0700

    TTbarSelection for CTag

--- python/TTbarSelectionFilter_cfi.py
+++ python/TTbarSelectionFilter_cfi.py
@@ -1,6 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 ttbarselectionfilter = cms.EDFilter('TTbarSelectionFilter',
-                                    selectChannels = cms.vint32(-11*11,-13*13,-11*13),
+                                    selectChannels = cms.vint32(-11*11,-13*13,-11*13), #dilepton channels
+				    #selectChannels = cms.vint32(-11,11,-13,13), #semileptonic channels
                                     selectAll      = cms.bool(False)
                                     )
