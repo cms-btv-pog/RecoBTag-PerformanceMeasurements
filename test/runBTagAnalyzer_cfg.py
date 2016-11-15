b0803a695b3cca0b7ed0070c6563b8548ff4aaef
Dinko Ferencek <Dinko.Ferencek@cern.ch>
2016-11-16 00:16:25 +0100

    fixed indentation

--- test/runBTagAnalyzer_cfg.py
+++ test/runBTagAnalyzer_cfg.py
@@ -1024,12 +1024,13 @@ if options.runOnData:
 ## Add GenParticlePruner for boosted b-tagging studies
 if not options.runOnData:
     process.prunedGenParticlesBoost = cms.EDProducer('GenParticlePruner',
-                                                     src = cms.InputTag(genParticles),
-                                                     select = cms.vstring("drop  *  ", #by default
-                                                                          "keep ( status = 3 || (status>=21 && status<=29) )", #keep hard process particles
-                                                                          "keep abs(pdgId) = 13 || abs(pdgId) = 15" #keep muons and taus
-                                                                          )
-                                                     )
+        src = cms.InputTag(genParticles),
+        select = cms.vstring(
+            "drop  *  ", #by default
+            "keep ( status = 3 || (status>=21 && status<=29) )", #keep hard process particles
+            "keep abs(pdgId) = 13 || abs(pdgId) = 15" #keep muons and taus
+        )
+    )
 
 #-------------------------------------
 
