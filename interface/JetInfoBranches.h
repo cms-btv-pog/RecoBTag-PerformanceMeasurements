74491484f130397b12c211ab369feb012004922d
Ivan Marchesini <ivan.marchesini@desy.de>
2017-01-18 18:29:23 +0100

    SetBranchAddress for Jet_uncorrpt

--- interface/JetInfoBranches.h
+++ interface/JetInfoBranches.h
@@ -995,6 +995,7 @@ class JetInfoBranches {
       if (name!="") name += ".";
       tree->SetBranchAddress((name+"nJet").c_str(),            &nJet           );
       tree->SetBranchAddress((name+"Jet_pt").c_str(),          Jet_pt          );
+      tree->SetBranchAddress((name+"Jet_uncorrpt").c_str(),          Jet_uncorrpt           );
       tree->SetBranchAddress((name+"Jet_genpt").c_str(),       Jet_genpt       );
       tree->SetBranchAddress((name+"Jet_residual").c_str(),    Jet_residual    );
       tree->SetBranchAddress((name+"Jet_area").c_str(),        Jet_area        );
