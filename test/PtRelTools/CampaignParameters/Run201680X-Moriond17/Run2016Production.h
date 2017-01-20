0f5bbe6b1f8b5ec4b3afe4f58ab3831e471292a6
scodella <luca.scodellaro@cern.ch>
2017-01-20 19:15:55 +0100

    Moriond17 information

---
+++ test/PtRelTools/CampaignParameters/Run201680X-Moriond17/Run2016Production.h
@@ -0,0 +1,5 @@
+TString Production = "_Run2016Production";
+
+const int nFitPtBins = 10;
+TString FitPtBin[nFitPtBins] = {"Pt2030", "Pt3050", "Pt5070", "Pt70100", "Pt100140", "Pt140200", "Pt200300", "Pt300600", "Pt6001000", "Pt1000"};
+double FitPtEdge[nFitPtBins+1] = {20., 30., 50., 70., 100., 140., 200., 300., 600., 1000., 1400.};
---
+++ test/PtRelTools/CampaignParameters/Run201680X-Moriond17/Taggers.h
@@ -0,0 +1,6 @@
+
+//const int nTaggers = 6;
+//TString TaggerName[nTaggers] = {"JPL", "JPM", "JPT", "CSVv2L", "CSVv2M", "CSVv2T"};
+
+const int nTaggers = 7;
+TString TaggerName[nTaggers] = {"JPT", "CSVv2L", "CSVv2M", "CSVv2T", "DeepCSVL", "DeepCSVM", "DeepCSVT"};
