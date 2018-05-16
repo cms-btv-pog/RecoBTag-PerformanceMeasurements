5a55c9360fd82aea63e59b4f7f8193fafaca9e5b
Johann Rauser <johann.rauser@cern.ch>
2018-05-16 18:03:10 +0200

    Update global tags and remove default values

--- python/defaults/Commissioning18.py
+++ python/defaults/Commissioning18.py
@@ -8,11 +8,11 @@ mc = {
         'inputFiles' : ['/store/relval/CMSSW_10_1_0_pre2/RelValTTbar_13/MINIAODSIM/100X_upgrade2018_realistic_v11-v1/20000/14AC524C-981E-E811-B6D1-0CC47A4D7632.root'],
         #'inputFiles' : ['/store/mc/RunIISpring18MiniAOD/TTToHadronic_TuneCP5_13TeV-powheg-pythia8/MINIAODSIM/100X_upgrade2018_realistic_v10-v1/30000/008B1124-0B31-E811-BD14-0025904C7A5A.root'], # Slightly older sample for testing
 	'JPCalibration' : 'JPcalib_MC94X_2017_v1',
-	'mcGlobalTag' : '94X_mc2017_realistic_v10',
+	'mcGlobalTag' : '94X_mc2017_realistic_v12',
 	}
 
 data = {
 	'inputFiles' : ['/store/data/Run2018A/JetHT/MINIAOD/PromptReco-v1/000/315/252/00000/04236AA8-404B-E811-9BD8-FA163EF52DAC.root'],
-	'JPCalibration' : 'JPcalib_Data94X_2017_v1',
+        'JPCalibration' : 'TrackProbabilityCalibration_PDF3D_express',
 	'dataGlobalTag' : '101X_dataRun2_Prompt_v9',
 }
