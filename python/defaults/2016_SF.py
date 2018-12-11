e0252199cad18948b86033b2fa36a18c952676de
Johann Rauser <johann.rauser@cern.ch>
2018-12-11 10:50:18 +0100

    Correct era

--- python/defaults/2016_SF.py
+++ python/defaults/2016_SF.py
@@ -1,11 +1,11 @@
 common = {
-	'eras' : ['Run2_2017'],
+	'eras' : ['Run2_2016','run2_miniAOD_80XLegacy'],
 	'miniAOD' : True,
 	'runDeepFlavourTagVariables' : True,
 }
 
 mc = {
-	'inputFiles' : ['/store/mc/RunIISummer16MiniAODv3/QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/110000/280DF6F8-7AD0-E811-884A-0025905AC97A.root'],
+	'inputFiles' : ['/store/mc/RunIISummer16MiniAODv3/QCD_Pt_80to120_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v1/110000/3A8373E0-30D3-E811-BB36-5CB901C2A510.root'],
 	'JPCalibration' : 'JPcalib_MC94X_2016_v1',
 	'mcGlobalTag' : '94X_mcRun2_asymptotic_v3',
 	}
