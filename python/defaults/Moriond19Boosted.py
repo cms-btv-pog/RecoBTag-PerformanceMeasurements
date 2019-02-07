af64e6df14d2a533d660e9d80e43e43d66b49390
Devdatta <devdatta.majumder@cern.ch>
2019-02-07 22:10:05 +0100

    Adding all subjet commissioning+SF+systematic variables; default config to run boosted commissioning

---
+++ python/defaults/Moriond19Boosted.py
@@ -0,0 +1,14 @@
+common = {
+	'eras' : ['Run2_2016'],
+	'miniAOD' : True,
+	'runDeepFlavourTagVariables' : False,
+  'doBoostedCommissioning': True,
+  'groups': ['EventInfo', 'Hadrons', 'PV', 'Devdatta'],
+}
+
+mc = {
+	'inputFiles' : ['/store/mc/RunIISummer16MiniAODv3/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v2/120000/92DEC622-A2E6-E811-9DE8-0025904CDDF8.root',],
+	'JPCalibration' : 'JPcalib_MC94X_2016_v1',
+	'mcGlobalTag' : '94X_mcRun2_asymptotic_v3',
+	}
+
