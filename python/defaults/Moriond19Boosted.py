6b01187f6fe74fc0cb9623049b348155b50c7eb2
Devdatta <devdatta.majumder@cern.ch>
2019-02-14 13:01:30 +0100

    Updating settings and GTs

--- python/defaults/Moriond19Boosted.py
+++ python/defaults/Moriond19Boosted.py
@@ -1,5 +1,5 @@
 common = {
-	'eras' : ['Run2_2016'],
+	'eras' : ['Run2_2018'],
 	'miniAOD' : True,
 	'runDeepFlavourTagVariables' : False,
   'doBoostedCommissioning': True,
@@ -7,13 +7,19 @@ common = {
 }
 
 mc = {
-	'inputFiles' : ['/store/mc/RunIISummer16MiniAODv3/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/MINIAODSIM/PUMoriond17_94X_mcRun2_asymptotic_v3-v2/120000/92DEC622-A2E6-E811-9DE8-0025904CDDF8.root',],
-	'JPCalibration' : 'JPcalib_MC94X_2016_v1',
-	'mcGlobalTag' : '94X_mcRun2_asymptotic_v3',
+	'inputFiles' : ['/store/mc/RunIIAutumn18MiniAOD/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCP5_13TeV_pythia8/MINIAODSIM/PUAvg50ForMUOVal_102X_upgrade2018_realistic_v15_ext1-v1/80000/1410E06E-D901-3448-9449-AB10C942CE03.root'],
+	'JPCalibration' : 'JPcalib_MC102X_2018_v1',
+	'mcGlobalTag' : '102X_upgrade2018_realistic_v12',
 	}
 
-data = {
-	'inputFiles' : ['/store/data/Run2016B/JetHT/MINIAOD/17Jul2018_ver2-v2/100000/00323A27-15B8-E811-88B5-E0071B6C9DB0.root'],	
-        'JPCalibration' : 'JPcalib_Data94X_2016_v1',
-	'dataGlobalTag' : '94X_dataRun2_v10',
+data17Sep2018 = {
+	'inputFiles' : ['/store/data/Run2018C/BTagMu/MINIAOD/17Sep2018-v1/60000/FE0C866E-6D00-DE47-89AB-A15CC16813B9.root'],
+  'JPCalibration' : 'JPcalib_Data102X_2018_v1',
+	'dataGlobalTag' : '102X_dataRun2_Sep2018Rereco_v1',
+}
+
+dataprompt = {
+	'inputFiles' : ['/store/data/Run2018D/BTagMu/MINIAOD/PromptReco-v2/000/325/022/00000/6F0A55A7-7F5F-A54D-A01E-243B27BBCB9A.root'],
+  'JPCalibration' : 'JPcalib_Data102X_2018_v1',
+	'dataGlobalTag' : '102X_dataRun2_Prompt_v11',
 }
