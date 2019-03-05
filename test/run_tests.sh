2062611013beb7c10da50c4e82af5700ac7e0b48
Johann Rauser <johann.rauser@cern.ch>
2019-03-05 11:24:50 +0100

    Forwarding changes of PR #116 to 10_5_X branch

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 set -o errexit
-function die { echo 'FAILED': status $1 ;  }
+function die { echo 'FAILED': status $1 ; }
 
 echo 'Running on data'
 cmsRun runBTagAnalyzer_cfg.py defaults=PixelStudies runOnData=True maxEvents=20 groups='test' &> last.log || die $?
@@ -32,6 +32,9 @@ cmsRun runBTagAnalyzer_cfg.py defaults=PixelStudies runOnData=False maxEvents=20
 
 # echo 'RECODEBUG'
 # cmsRun runBTagAnalyzer_cfg.py defaults=PixelStudies runOnData=False maxEvents=20 miniAOD=False runFatJets=True runSubJets=True useTrackHistory=True produceJetTrackTree=True inputFiles=/store/mc/RunIIFall17DRPremix/QCD_Pt_15to30_TuneCP5_13TeV_pythia8/GEN-SIM-RECODEBUG/94X_mc2017_realistic_v10-v1/70000/04F991F0-C2DD-E711-B302-0CC47A78A3F8.root groups='testfat' &> last.log || die $?
+
+echo 'Running on MINIAODSIM boosted'
+cmsRun runBTagAnalyzer_cfg.py defaults=Moriond19Boosted runOnData=False maxEvents=20 &> boosted.log || die $? 
 
 
 
