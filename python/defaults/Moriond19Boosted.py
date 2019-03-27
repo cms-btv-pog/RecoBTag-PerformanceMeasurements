9bc628603a22905d33ce63bbbc8f5bf4cadfd48f
Devdatta <devdatta.majumder@cern.ch>
2019-03-27 20:33:47 +0100

    option for applying private JECs turned on

--- python/defaults/Moriond19Boosted.py
+++ python/defaults/Moriond19Boosted.py
@@ -10,13 +10,17 @@ mc = {
 	'inputFiles' : ['/store/mc/RunIIAutumn18MiniAOD/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCP5_13TeV_pythia8/MINIAODSIM/PUAvg50ForMUOVal_102X_upgrade2018_realistic_v15_ext1-v1/80000/1410E06E-D901-3448-9449-AB10C942CE03.root'],
 	'JPCalibration' : 'JPcalib_MC102X_2018_v1',
 	'mcGlobalTag' : '102X_upgrade2018_realistic_v12',
+  'usePrivateJEC': True,
+  'jecDBFileMC': 'Autumn18_V8_MC'
 	}
 
 ####17Sep2018
 #data = {
 #	'inputFiles' : ['/store/data/Run2018C/BTagMu/MINIAOD/17Sep2018-v1/60000/FE0C866E-6D00-DE47-89AB-A15CC16813B9.root'],
-#  'JPCalibration' : 'JPcalib_Data102X_2018_v1',
+# 'JPCalibration' : 'JPcalib_Data102X_2018_v1',
 #	'dataGlobalTag' : '102X_dataRun2_Sep2018Rereco_v1',
+# 'usePrivateJEC': True,
+# 'jecDBFileData': 'Autumn18_RunABCD_V8_DATA'
 #}
 
 ###Prompt
