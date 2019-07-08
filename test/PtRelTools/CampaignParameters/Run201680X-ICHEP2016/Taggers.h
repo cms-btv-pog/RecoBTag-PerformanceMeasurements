b0b0c2e20e484ca519dee241e01b0261c3c7f85d
Johann Rauser <38247774+rauser@users.noreply.github.com>
2019-07-08 16:41:40 +0200

    Merge pull request #132 from rauser/CodeCleanup
    Code Cleanup

--- test/PtRelTools/CampaignParameters/Run201680X-ICHEP2016/Taggers.h
+++
@@ -1,6 +0,0 @@
-
-//const int nTaggers = 6;
-//TString TaggerName[nTaggers] = {"JPL", "JPM", "JPT", "CSVv2L", "CSVv2M", "CSVv2T"};
-
-const int nTaggers = 4;
-TString TaggerName[nTaggers] = {"JPT", "CSVv2L", "CSVv2M", "CSVv2T"};
--- test/ttbar/KIN_runClassifier.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-INPUTFILE="analysis/MC13TeV_TTJets.root"
-
-a=(0 1 2)
-for i in ${a[@]}; do
-    root -b -q "${CMSSW_BASE}/src/RecoBTag/PerformanceMeasurements/test/ttbar/KIN_trainClassifier.C+(\"BDT\",\"${INPUTFILE}\",${i})";
-    mv TMVA.root TMVA_${i}.root;
-done
