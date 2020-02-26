7bcac3c4890f161e3ede213139313404f2c116bc
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2020-02-26 14:51:45 +0100

    updates for CMSSW11_0_0 RelVals

--- test/submit_allHLTMC.sh
+++ test/submit_allHLTMC.sh
@@ -6,6 +6,6 @@ python submit_all.py \
   runBTagAnalyzer_cfg.py \
   -f CRAB/tosubmit.txt \
   -s T2_DE_DESY \
-  -p defaults=PhaseII runOnData=False  \
+  -p defaults=PhaseII_CMSSW_11_0_0 runOnData=False runCMSSW11Sample=True \
   -o /store/user/sewuchte/BTagServiceWork/PhaseII/Offline/ \
-  -v crab_CMSSW11_NewGT_v1_TT
+  -v crab_NewOfflineSequence_CMSSW11_0_0_TrackingV0_1
