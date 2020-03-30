93d398b1ebe248360a38c82eddf9ec3d04fc795b
Sebastian Wuchterl <sebastian.wuchterl@rwth-aachen.de>
2020-03-30 14:07:55 +0200

    change inputTestfile

--- python/defaults/PhaseII.py
+++ python/defaults/PhaseII.py
@@ -10,6 +10,6 @@ common = {
 	'maxJetEta' : 4.5,
 	'usePrivateJEC' : True,
 	'jecDBFileMC' : 'PhaseIIFall17_V5b_MC',
-	'inputFiles' : ['/store/mc/Phase2HLTTDRWinter20RECOMiniAOD/TTTo2L2Nu_TuneCP5_14TeV-powheg-pythia8/MINIAODSIM/PU200_110X_mcRun4_realistic_v3-v2/40000/EE7A44F9-F668-E74B-AAC3-059BFB8B2168.root',],
-	# 'JPCalibration' : 'JPcalib_MC81X_v0',
+	'inputFiles' : ['/store/mc/Phase2HLTTDRWinter20RECOMiniAOD/TTToSemiLepton_TuneCP5_14TeV-powheg-pythia8/MINIAODSIM/NoPU_110X_mcRun4_realistic_v3-v2/20000/F3F1773B-D087-F941-B4EE-40F14D00A794.root',],
+    # 'JPCalibration' : 'JPcalib_MC81X_v0',
 }
